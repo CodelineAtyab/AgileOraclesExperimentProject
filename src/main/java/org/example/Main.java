4278ec1b6a477327d175fe3c476abb9be7c6ee38
Syed Atyab Hussain <119934383+CodelineAtyab@users.noreply.github.com>
2026-02-23 09:38:03 +0400

    Initial commit with an empty project and a basic menu (#9)

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("============= MENU =============");
+        System.out.println("1. View Tickets");
+        System.out.println("2. Update Tickets");
+        System.out.println("3. Delete Tickets");
+        System.out.println("4. View Specific Ticket");
+        System.out.println("============= **** =============");
+
+    }
+}
