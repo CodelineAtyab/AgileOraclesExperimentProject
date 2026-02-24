0823827672e00541ee1e094fa326503fbc370c51
likawther3li <likawther17@gmail.com>
2026-02-24 08:53:52 +0400

    add main class (#27)
    * add main class
    
    * examples from kawther

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-        For now lets leave it empty as this would be the main entry point of the project
-        Each team member can go to their example package and run the Program independently
-        Which means having a main method for each class created.
-         */
+
         System.out.println("The Parent Project Main");
     }
 }
