2e5deb3ea388a363dd40b57368f478e937c4525c
alharithalk <kindaking99@gmail.com>
2026-02-24 08:02:37 +0400

    added to main

---
+++ src/main/java/org/example/fromalharithAlkindi/Main.java
@@ -0,0 +1,10 @@
+package org.example.fromalharithAlkindi;
+
+public class Main {
+    public static void main (String[] args){
+        System.out.println("===== Start My Program =====");
+
+
+
+    }
+}
