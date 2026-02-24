c5882e72abbe31a372aee12a9a5369d83bc53324
Ahed Albarhi <iiihed7@gmail.com>
2026-02-24 08:51:27 +0400

    updated

--- src/main/java/org/example/Ahed/Main.java
+++ src/main/java/org/example/Ahed/Main.java
@@ -1,8 +1,7 @@
 package org.example.Ahed;
 
 public class Main {
-
-    public static void main (String [] args){
+    public static void main(String[] args) {
         System.out.println("Ahed");
     }
 }
