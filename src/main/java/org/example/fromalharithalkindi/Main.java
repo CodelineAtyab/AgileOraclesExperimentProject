d532d6f442bdf18ab1bdee190620525940fb958e
AL-Harith AL-Kindi <129694811+alharithalk@users.noreply.github.com>
2026-02-24 09:03:48 +0400

    added some start sout to my  code  (#20)
    * added to main
    
    * renamed folder name
    
    * renamed folder name again

---
+++ src/main/java/org/example/fromalharithalkindi/Main.java
@@ -0,0 +1,10 @@
+package org.example.fromalharithalkindi;
+
+public class Main {
+    public static void main (String[] args){
+        System.out.println("===== Start My Program =====");
+
+
+
+    }
+}
