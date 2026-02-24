03da07180d3880cd2745dcf47a74025bc08c6ad7
AbdullahAlHadabi <Abdullah.Juma280@hotmail.com>
2026-02-24 08:19:46 +0400

    add new print

--- src/main/java/org/example/fromabdullahalhadabi/Main.java
+++ src/main/java/org/example/fromabdullahalhadabi/Main.java
@@ -3,5 +3,6 @@ package org.example.fromabdullahalhadabi;
 public class Main {
     public static void main(String [] agrs){
         System.out.println("Hello there");
+        System.out.println("Hello there");
     }
 }
