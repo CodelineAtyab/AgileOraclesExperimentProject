7608957e17e3f3906cf6d014d7a2ead153c9ef54
AbdullahAlHadabi <Abdullah.Juma280@hotmail.com>
2026-02-24 08:38:35 +0400

    Just adding a work (#14)
    * Adding Main
    
    * add new print

---
+++ src/main/java/org/example/fromabdullahalhadabi/Main.java
@@ -0,0 +1,8 @@
+package org.example.fromabdullahalhadabi;
+
+public class Main {
+    public static void main(String [] agrs){
+        System.out.println("Hello there");
+        System.out.println("Hello there");
+    }
+}
