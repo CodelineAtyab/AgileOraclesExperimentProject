0064c25f7a5a28e3aa150f55f67e850acefe8e00
Your Name <you@example.com>
2026-02-24 08:57:19 +0400

    Updated by Alharith

--- src/main/java/org/example/fromalharith/Main.java
+++ src/main/java/org/example/fromalharith/Main.java
@@ -1,9 +1,6 @@
-package org.example.Alharith;
-
+package org.example.fromalharith;
 public class Main {
-
     public static void main(String[] args) {
-
         System.out.println("Check 1 Check 2");
     }
 }
