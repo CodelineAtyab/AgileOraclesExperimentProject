f28cf05994e9d192a0151b8b05f52ceaf6997dd0
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-02-24 08:05:37 +0400

    Branch Added by saif

---
+++ src/main/java/org/example/fromsaif/Main.java
@@ -0,0 +1,9 @@
+package org.example.fromsaif;
+
+public class Main {
+    public static void main(String[] args) {
+
+            System.out.println("Saif Main Project");
+            System.out.println("Example from Saif");
+        }
+}
