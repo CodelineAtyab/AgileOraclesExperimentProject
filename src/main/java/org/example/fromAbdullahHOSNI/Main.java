c906da4a204eaa684ac8dd7d314cab1fb00e7545
abbhosni <binkhalidabdulahv@gmail.com>
2026-02-24 08:02:18 +0400

    Added Main class

---
+++ src/main/java/org/example/fromAbdullahHOSNI/Main.java
@@ -0,0 +1,8 @@
+package org.example.fromAbdullahHOSNI;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Agile project ");
+        System.out.println("Examples from Abdullah");
+    }
+}
