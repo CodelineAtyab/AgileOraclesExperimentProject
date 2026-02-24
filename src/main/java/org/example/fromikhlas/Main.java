01c27df3c50ce8c347e2a3b71469e1885914f01c
Syed Atyab Hussain <syed.atyab.hussain.om@gmail.com>
2026-02-24 08:18:59 +0400

    Updated Main

--- src/main/java/org/example/fromikhlas/Main.java
+++ src/main/java/org/example/fromikhlas/Main.java
@@ -3,5 +3,6 @@ package org.example.fromikhlas;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Examples from Ikhlas");
+        System.out.println("Examples from Ikhlas");
     }
 }
