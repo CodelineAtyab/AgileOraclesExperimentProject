7ab02987df7dd218ad0f333f65c899ef2cadb2ff
abdulmajeedalbalushi <codeline@example.com>
2026-02-24 08:21:23 +0400

    update

--- src/main/java/org/example/formabdulmajeed/Main.java
+++ src/main/java/org/example/formabdulmajeed/Main.java
@@ -3,5 +3,6 @@ package org.example.formabdulmajeed;
 public class Main {
     public static void main(String[] args){
         System.out.println("Examples form Abdulmajeed");
+        System.out.println("Examples 2 form Abdulmajeed");
     }
 }
