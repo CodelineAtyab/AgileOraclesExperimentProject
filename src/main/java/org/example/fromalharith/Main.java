cfb35ae75bca637e8ca6395069f80051f01c64d8
Your Name <you@example.com>
2026-02-25 07:25:48 +0400

    alharithmenu

--- src/main/java/org/example/fromalharith/Main.java
+++ src/main/java/org/example/fromalharith/Main.java
@@ -1,6 +1,41 @@
 package org.example.fromalharith;
+
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Check 1 Check 2");
+        boolean name = true;
+
+        do {
+
+            int menu1 = scanner.nextInt();
+            int menu = menu1;
+
+            switch (menu) {
+                case 1:
+                    System.out.println("1. Customer");
+                    break;
+
+                case 2:
+                    System.out.println("2. Admin");
+                    break;
+
+                case 3:
+                    System.out.println("3. Support Staff");
+                    break;
+
+                case 4:
+                    System.out.println("4. Exit");
+                    name = false;
+                    break;
+
+            }
+        }
+
+        while (name) ;
+        scanner.close();
+
     }
 }
