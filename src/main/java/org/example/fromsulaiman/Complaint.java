b7937507a7685719b01e30a002a5d9467ac87e1f
SulaimanAlfarsi <sulaimanalfarsi3060@gmail.com>
2026-02-25 09:54:40 +0400

    Implement Admin module with PIN authentication, complaint viewing, search, and status management

--- src/main/java/org/example/fromsulaiman/Complaint.java
+++ src/main/java/org/example/fromsulaiman/Complaint.java
@@ -1,23 +1,36 @@
 public class Complaint {
 
-    private int complainId;
-    private String description;
+    private int complaintId;    private String description;
     private String priority;
+    private String status;
 
     public Complaint(int complainId, String description, String priority){
-        this.complainId =complainId;
+        this.complaintId =complainId;
         this.description= description;
         this.priority =priority;
     }
 
-    public int getComplainId(){
-        return complainId;
+    public int getComplaintId(){
+        return complaintId;
     }
     public String getDescription(){
         return description;
     }
     public String getPriority(){
         return priority;
+    }
+    public String getStatus(){
+        return status;
+    }
+    public void closeComplaint(){
+        this.status = "CLOSED";
+    }
+    @Override
+    public String toString(){
+        return "Complaint ID: " + complaintId +
+                "\nDescription: " + description +
+                "\nPriority: " + priority +
+                "\nStatus: " + status;
     }
 
 
--- src/main/java/org/example/fromsulaiman/Main.java
+++ src/main/java/org/example/fromsulaiman/Main.java
@@ -3,9 +3,14 @@ import java.util.Scanner;
 
 
 public class Main {
+
+    private static final String ADMIN_PIN="1234";
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Complaint> complaints =new ArrayList<>();
+
         boolean isRunning = true;
 
         while (isRunning) {
@@ -16,24 +21,32 @@ public class Main {
             System.out.println("2. Admin");
             System.out.println("3. Support Staff");
             System.out.println("4. Exit");
+            System.out.println("Choose an option: ");
 
             int choice;
 
             try {
-                choice = scanner.nextInt();
+                choice = Integer.parseInt(scanner.nextLine());
             } catch (Exception e) {
                 System.out.println("Invalid input! Please enter numbers only.");
-                scanner.nextLine(); // clear wrong input
-                continue; // go back to menu
+                continue;
             }
 
             switch (choice) {
                 case 1:
-                    System.out.println("Customer module selected.");
+                    createComplaint(scanner,complaints);
                     break;
 
                 case 2:
-                    System.out.println("Admin module selected.");
+                    System.out.println("Enter Admin PIN: ");
+                    String pin =scanner.nextLine();
+
+                    if (pin.equals(ADMIN_PIN)){
+                        System.out.println("Login successful ");
+                        adminLogin(scanner,complaints);
+                    }else {
+                        System.out.println("Incorrect PIN. Access Denied");
+                    }
                     break;
 
                 case 3:
@@ -71,7 +84,7 @@ public class Main {
         System.out.println("Select Priority: ");
         System.out.println("1. Low");
         System.out.println("2. Medium");
-        System.out.println("1. High");
+        System.out.println("3. High");
         System.out.println("Choose option (1-3 pr press Enter for Default");
 
         String priority = "Medium"; //default
@@ -96,4 +109,116 @@ public class Main {
         System.out.println("Complaint Created Successfully ");
         System.out.println(complaint);
     }
+    // Admin Login
+
+    public static void adminLogin(Scanner scanner,ArrayList<Complaint> complaints){
+        boolean isAdminRunning =true;
+
+        while (isAdminRunning){
+
+            System.out.println("=====Admin Menu=====");
+            System.out.println("1. View All Complaints");
+            System.out.println("2. Search Complaint by ID");
+            System.out.println("3. Close Complaint");
+            System.out.println("4. Back to Main Menu");
+            System.out.println("Choose option: ");
+
+
+            int choice;
+            try{
+                choice =Integer.parseInt(scanner.nextLine());;
+            }catch (Exception e){
+                System.out.println("Invalid input! Numbers only");
+                continue;
+            }
+
+            switch (choice){
+                case 1:
+                    viewAllComplaints(complaints);
+                    break;
+
+                case 2:
+                    searchComplaint(scanner,complaints);
+                    break;
+
+                case 3:
+                    closeComplaint(scanner,complaints);
+                    break;
+
+                case 4:
+                    isAdminRunning =false;
+                    break;
+
+                default:
+                    System.out.println("Invalid option");
+            }
+        }
+    }
+
+    // View All Complaints
+
+    public static void viewAllComplaints(ArrayList<Complaint> complaints){
+
+        if (complaints.isEmpty()){
+            System.out.println("No complaints found.");
+            return;
+        }
+
+        for (Complaint complaint: complaints){
+            System.out.println("--------------------");
+            System.out.println(complaint);
+        }
+    }
+
+    // Search by ID
+    public static void searchComplaint(Scanner scanner, ArrayList<Complaint> complaints){
+        System.out.println("Enter Complaint ID to search: ");
+
+        int id;
+
+        try {
+            id = Integer.parseInt(scanner.nextLine());
+        }catch (Exception e){
+            System.out.println("Invalid ID");
+            return;
+        }
+
+        for (Complaint complaint: complaints){
+            if (complaint.getComplaintId()==id){
+                System.out.println("Complaint Found: ");
+                System.out.println(complaint);
+                return;
+            }
+        }
+        System.out.println("Complaint not Found");
+    }
+
+    // Close Complaint
+
+    public static void closeComplaint(Scanner scanner ,ArrayList<Complaint>complaints){
+
+        System.out.println("Enter Complaint ID to close: ");
+
+        int id;
+
+        try {
+            id= Integer.parseInt(scanner.nextLine());
+        }catch (Exception e ){
+            System.out.println("Invalid ID");
+            return;
+        }
+        for (Complaint complaint: complaints){
+            if (complaint.getComplaintId()== id ){
+                if(complaint.getStatus().equals("CLOSED")){
+                    System.out.println(" Complaint already Closed");
+                }else {
+                    complaint.closeComplaint();
+                    System.out.println(" Complaint closed successfully");
+                }
+                return;
+            }
+        }
+        System.out.println("Complaint Not Found");
+    }
+
 }
