654a896ce90aa0b80da9d52e0d8c6da158b8c360
Your Name <you@example.com>
2026-02-26 11:05:03 +0400

    ticket 2 uploaded

--- src/main/java/org/example/fromalharith/Main.java
+++ src/main/java/org/example/fromalharith/Main.java
@@ -1,32 +1,116 @@
 package org.example.fromalharith;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    static Scanner scanner = new Scanner(System.in);
+    static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
         boolean name = true;
 
         do {
 
-            int menu1 = scanner.nextInt();
-            int menu = menu1;
+            String menu = scan.nextLine();
 
             switch (menu) {
-                case 1:
+                case "1":
                     System.out.println("1. Customer");
+                    Scanner sc= new Scanner(System.in);
+
+                    // ArrayList for Complaints
+
+                    ArrayList<Integer> complaintIds = new ArrayList<>();
+                    ArrayList<String> complaintDescriptions = new ArrayList<>();
+                    ArrayList<String> complaintPriorities = new ArrayList<>();
+
+
+                    System.out.println("<Please Enter Your Complaint>");
+
+
+
+                    boolean complaint = true;
+                    int idCounter = 1;
+
+                    while(complaint){
+
+                        System.out.println("   1. Create Complaint");
+                        System.out.println("   2. View Complaints");
+                        System.out.println("   3. Exit");
+                        System.out.print("Choose an Option: ");
+
+
+                        String choice = sc.nextLine();
+
+                        if(choice.equals("1")){
+
+                            System.out.println("Add your Complaint ");
+                            String des = sc.nextLine();
+
+
+                            System.out.print("Enter priority (Low / Medium / High): ");
+                            String priority = sc.nextLine();
+
+
+                            complaintIds.add(idCounter);
+                            complaintDescriptions.add(des);
+                            complaintPriorities.add(priority);
+
+                            idCounter++;
+
+
+                        } else if (choice.equals("2")) {
+
+                            if (complaintIds.isEmpty()) {
+                                System.out.println(" No complaints found.");
+
+                            } else {
+
+                                System.out.println("\n--- Complaints List ---");
+                                for (int i = 0; i < complaintIds.size(); i++) {
+
+                                    System.out.println(
+
+                                            "ID: " + complaintIds.get(i)
+
+                                                    + " | Description: " + complaintDescriptions.get(i)
+
+                                                    + " | Priority: " + complaintPriorities.get(i)
+
+                                    );
+
+                                }
+
+                            }
+
+
+                        } else if (choice.equals("3")) {
+
+                            complaint = false;
+
+                            System.out.println("Exiting system...");
+
+                        } else {
+                            System.out.println("Invalid choice!");
+
+                        }
+                    }
+
+                    // sc.close();
+
+
+
                     break;
 
-                case 2:
+                case "2":
                     System.out.println("2. Admin");
                     break;
 
-                case 3:
+                case "3":
                     System.out.println("3. Support Staff");
                     break;
 
-                case 4:
+                case "4":
                     System.out.println("4. Exit");
                     name = false;
                     break;
@@ -35,7 +119,9 @@ public class Main {
         }
 
         while (name) ;
-        scanner.close();
+        scan.close();
+
+
 
     }
 }
