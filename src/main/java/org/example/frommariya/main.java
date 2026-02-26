c25f2c8facb35574b9d946e5337c215eae9f5695
Mariya <lamkia2001@gmail.com>
2026-02-26 12:07:03 +0400

    Worked in task 1-Project Setup and Main Menu

--- src/main/java/org/example/frommariya/main.java
+++ src/main/java/org/example/frommariya/main.java
@@ -1,7 +1,36 @@
 package org.example.frommariya;
 
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args){
-        System.out.println("Examples from Mariya");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true){
+            System.out.println("1. Customer");
+            System.out.println("2. Admin ");
+            System.out.println("3. Support Staff");
+            System.out.println("4. EXIT");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            if ( choice == 1 ){
+                System.out.println("Customer...");
+            }
+            else if ( choice == 2 ){
+                System.out.println("Admin...");
+            }
+            else if ( choice == 3 ){
+                System.out.println("Support...");
+            }
+            else {
+                break;
+            }
+        }
+        System.out.println("BYE!");
+        scanner.close();
+
     }
 }
