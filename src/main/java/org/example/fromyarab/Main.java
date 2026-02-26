f8480394a322ddabc525d3b212a662d6be5f4d04
Ya'Rab Al-Mamari <115209826+JuniorPrice@users.noreply.github.com>
2026-02-26 08:44:17 +0400

    Project-Setup-and-Main-Menu (#34)

--- src/main/java/org/example/fromyarab/Main.java
+++ src/main/java/org/example/fromyarab/Main.java
@@ -1,8 +1,56 @@
 package org.example.fromyarab;
 
-public class Main {
+
+import java.util.Scanner;
+
+public class Main{
     public static void main(String[] args){
-        System.out.println("Hello from yarab !!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("=".repeat(50));
+        System.out.println("                   --- CLI ERP ---   ");
+        System.out.println("=".repeat(50));
+
+        boolean con = true;
+        int choice = 0;
+        while (con){
+            System.out.println("   --- Main menu ---\n");
+            System.out.println("1. Customer");
+            System.out.println("2. Admin");
+            System.out.println("3. Support Staff");
+            System.out.println("4. Exit");
+            System.out.print("\nEntr your choice: ");
+            choice = scan.nextInt();
+
+            switch (choice) {
+                case 1 -> {
+                    Customer();
+                }
+                case 2 -> {
+                    Admin();
+                }
+                case 3 -> {
+                    Support_Staff();
+                }
+                case 4 -> {
+                    System.out.println("GoodBay!...");
+                    con = false;
+                }
+                default -> System.out.println("Invalid Input!!.. ");
+            }
+        }
     }
+
+    static void Customer(){
+
+    }
+
+    static void Admin(){
+
+    }
+
+    static void Support_Staff(){
+
+    }
+
 
 }
