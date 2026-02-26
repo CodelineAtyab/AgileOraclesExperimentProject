f9dba75ca7378852061abe1789d3a005db868445
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-02-26 11:48:28 +0400

    Customer Module: Create Complaint

--- src/main/java/org/example/fromaljolanda/Main.java
+++ src/main/java/org/example/fromaljolanda/Main.java
@@ -1,5 +1,6 @@
 package org.example.fromaljolanda;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -7,6 +8,11 @@ public class Main {
 
         Scanner input = new Scanner(System.in); // Scanner to read user input
         boolean mainMenuRunning = true; // Controls Main Menu Loop
+
+        ArrayList<Integer> complaintIds = new ArrayList<>();
+        ArrayList<String> complaintDescriptions = new ArrayList<>();
+        ArrayList<String> complaintPriorities = new ArrayList<>();
+
         // ================= MAIN MENU =================
         while (mainMenuRunning) {
             System.out.println("================= MAIN MENU =================");
@@ -19,10 +25,45 @@ public class Main {
 
             // Check if user entered a number
             if (input.hasNextInt()) {
-                int userChoice = Integer.parseInt(input.next());
+                int userChoice = Integer.parseInt(input.nextLine());
                 // ================= Customer MENU =================
                 if (userChoice == 1) {
                     System.out.println("Customer Module Selected.");
+                    System.out.print("Enter Complaint ID (numeric): "); // Ask User to Enter Complaint ID
+                    if (input.hasNextInt()) {
+                        int complaintId = input.nextInt();
+                        input.nextLine();
+
+                        System.out.print("Enter Complaint Description: "); // Ask User to Enter Complaint Description
+                        String description = input.nextLine();
+
+                        System.out.print("Select Priority (LOW / MEDIUM / HIGH) or press Enter for default (MEDIUM): "); // Ask User to Enter Complaint Priority
+                        String priority = input.nextLine().toUpperCase();
+
+                        if (priority.isEmpty()) {
+                            priority = "MEDIUM"; // Default = MEDIUM
+                        }
+                        if (!priority.equals("LOW") && !priority.equals("MEDIUM") && !priority.equals("HIGH")) {
+                            System.out.println("Invalid priority! Setting to default (MEDIUM)");
+                            priority = "MEDIUM";
+                        }
+
+                        // Store Data
+                        complaintIds.add(complaintId);
+                        complaintDescriptions.add(description);
+                        complaintPriorities.add(priority);
+
+                        System.out.println();
+                        System.out.println("Complaint created successfully!"); // Confirmation message
+                        System.out.println("---> Complaint details:");
+                        System.out.println("Complaint ID: " + complaintId);
+                        System.out.println("Complaint Description: " + description);
+                        System.out.println("Complaint Priority: " + priority);
+                    }
+                    else {
+                        System.out.println("Invalid ID! Must be numeric.");
+                        input.nextLine(); // clear invalid input
+                    }
                 }
                 // ================= Admin MENU =================
                 else if (userChoice == 2) {
