c2181203dec0645e33466f855b48808139a88021
TibyanSaad <saad.tibyan02@gmail.com>
2026-02-26 12:02:41 +0400

    fixed all customer input validation

--- src/main/java/org/example/fromtibyan/CLIERP/CustomerMenu.java
+++ src/main/java/org/example/fromtibyan/CLIERP/CustomerMenu.java
@@ -24,36 +24,39 @@ public class CustomerMenu {
         System.out.println("Enter complaint description: ");
         String compDes = userInput.nextLine();
 
-        // fix this block for invalid input
+        String comPri;
+        while (true) {
             System.out.println("Enter complaint Priority. \n1.LOW 2.MEDIUM (default) 3.HIGH ");
-            String comPri = userInput.nextLine().trim();
-            int compriIndex = comPri.isEmpty() ? 2 : Integer.parseInt(comPri);
-            if (compriIndex == 1) {
-                comPri = "LOW";
-            }
-            else if (compriIndex == 2) {
+            String priority = userInput.nextLine();
+
+            if (priority.isEmpty()) {
                 comPri = "MEDIUM";
+                break;
             }
-            else if (compriIndex == 3) {
+
+            int comPriIndex;
+            try {
+                comPriIndex = Integer.parseInt(priority);
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid complaint priority number.");
+                continue;
+            }
+
+            if (comPriIndex == 1) {
+                comPri = "LOW";
+                break;
+            } else if (comPriIndex == 2) {
+                comPri = "MEDIUM";
+                break;
+            } else if (comPriIndex == 3) {
                 comPri = "HIGH";
+                break;
             } else {
-                System.out.println("Please select a valid number.");
+                System.out.println("Please select a valid priority number 1-3.");
             }
-        // fix this block for invalid input
+        }
 
         Tickets.add(new Complaints(compID, compDes, comPri));
         System.out.println("Ticket Created Successfully!");
-
-        userInput.nextLine();
-        System.out.println("Ticket number to be viewed: ");
-        int adminCompID = Integer.parseInt(userInput.nextLine());
-
-        for (Complaints adminTicketId : Tickets) {
-            if (adminTicketId.getComplaintId() == adminCompID) {
-                System.out.println("Ticket Number: " + adminTicketId.getComplaintId());
-                System.out.println("Description: " + adminTicketId.getComplaintDescription());
-                System.out.println("Priority: " + adminTicketId.getComplaintPriority());
-            }
-        }
     }
 }
