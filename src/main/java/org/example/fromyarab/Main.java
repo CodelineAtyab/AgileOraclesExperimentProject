0a0eb975357b6dfd908a1647e9012488f489365e
Ya'Rab Al-Mamari <yarop123456789@gmail.com>
2026-03-01 12:25:30 +0400

    Admin Module

--- src/main/java/org/example/fromyarab/Main.java
+++ src/main/java/org/example/fromyarab/Main.java
@@ -35,7 +35,7 @@ public class Main{
                 Customer(complaints);
             }
             else if (choice == 2){
-                Admin();
+                Admin(complaints);
             }
             else if (choice == 3){
                 Support_Staff();
@@ -84,7 +84,8 @@ public class Main{
         // save the compliant
         complaint.put("ID" , id);
         complaint.put("Description" , description);
-        complaint.put("priority" , priority);
+        complaint.put("Priority" , priority);
+        complaint.put("Status" , "OPEN");
         complaints.add(complaint);
 
         System.out.println("\nYour Complaint Has Been Recorded Successfully >...\n");
@@ -92,8 +93,140 @@ public class Main{
     }
 
     // Admin Module
-    static void Admin(){
+    static void Admin(ArrayList<Map<String, Object>> complaints){
+        Scanner scan = new Scanner(System.in);
 
+        // validate admin access
+        System.out.print("Enter Administrator PIN Number: ");
+        String SPIN = scan.nextLine();
+        if (!isNumeric(SPIN) || Integer.parseInt(SPIN) != 1234){
+            System.out.println("Invalid PIN Number!!");
+            System.out.println("Returning to main menu ...\n");
+            return;
+        }
+
+        System.out.println();
+
+        // display the administrator menu
+        boolean active = true;
+        while (active){
+            // display main menu
+            System.out.println("   --- Administrator Menu ---");
+            System.out.println("1. View all complaints");
+            System.out.println("2. Search complaint by ID");
+            System.out.println("3. Close a complaint");
+            System.out.println("4. Exit Administrator menu");
+            System.out.print("\nEnter your choice: ");
+            String sChoice = scan.nextLine();
+            int choice = 0;
+
+            // check user input
+            if (isNumeric(sChoice)){
+                choice = Integer.parseInt(sChoice);
+            }
+
+            // handle user choice
+            if (choice == 1){
+                if (complaints.isEmpty()) {
+                    System.out.println("NO Complaints Found ..\n");
+                }
+                else {
+                    System.out.println("List of complaints: ");
+                    int counter = 0;
+                    for (Map complaint : complaints){
+                        System.out.println(counter + ". Complaint ID: " + complaint.get("ID"));
+                        System.out.println("   Complaint Description: " + complaint.get("Description"));
+                        System.out.println("   Complaint Priority: " + complaint.get("Priority"));
+                        System.out.println("   Complaint Status: " + complaint.get("Status"));
+                        System.out.println();
+                    }
+                }
+            }
+            else if (choice == 2){
+                // check if no complaints yet
+                if (complaints.isEmpty()) {
+                    System.out.println("Sorry! NO Complaints Found ..\n");
+                }
+                else {
+                    System.out.print("Enter Complaint ID: ");
+                    String sID = scan.nextLine();
+                    if (!isNumeric(sID)){
+                        System.out.println("Error! Please Numbers Are Only Allowed For Complaint ID ..");
+                        System.out.println("Returning to Administrator Menu >>>\n");
+                    }
+                    else {
+                        // handle complaint id
+                        int ID = Integer.parseInt(sID);
+                        int found = 0;
+                        Map<Object, Object> searchedComplaint = new HashMap<>();
+                        for (Map complaint : complaints){
+                            if (complaint.get("ID").equals(ID)){
+                                searchedComplaint = complaint;
+                                found = 1;
+                            }
+                        }
+                        // print the complaint info
+                        if (found == 1){
+                            System.out.println(" -- Complaint Info --");
+                            System.out.println("Complaint ID: "+ searchedComplaint.get("ID"));
+                            System.out.println("Complaint Description: " + searchedComplaint.get("Description"));
+                            System.out.println("Complaint Priority: " + searchedComplaint.get("Priority"));
+                            System.out.println();
+                        }
+                        else {
+                            System.out.printf("There is no complaint with ID number (%d) \n\n" ,ID);
+                        }
+                    }
+                }
+
+            }
+            else if (choice == 3){
+                // check if no complaints yet
+                if (complaints.isEmpty()) {
+                    System.out.println("Sorry! NO Complaints Found ..\n");
+                }
+                else {
+                    System.out.print("Enter Complaint ID to Close: ");
+                    String sID = scan.nextLine();
+                    if (!isNumeric(sID)){
+                        System.out.println("Error! Please Numbers Are Only Allowed For Complaint ID ..");
+                        System.out.println("Returning to Administrator Menu >>>\n");
+                    }
+                    else {
+                        // handle complaint id
+                        int ID = Integer.parseInt(sID);
+                        int found = 0;
+                        int index = 0;
+                        int cout = 0;
+                        Map<String, Object> closeComplaint = new HashMap<>();
+                        for (Map complaint : complaints){
+                            if (complaint.get("ID").equals(ID)) {
+                                closeComplaint = complaint;
+                                index = cout;
+                                found = 1;
+                            }
+                            cout = cout + 1;
+                        }
+                        // change the status to closed
+                        if (found == 1){
+                            closeComplaint.replace("Status" , "CLOSED");
+                            complaints.set(index , closeComplaint);
+                            System.out.println("The Complaint Closed Successfully >...\n");
+                        }
+                        else {
+                            System.out.printf("There is no complaint with ID number (%d) \n\n" ,ID);
+                        }
+                    }
+                }
+            }
+            else if(choice == 4){
+                System.out.println("Exiting Administrator menu!...\n");
+                active = false;
+            }
+            else {
+                System.out.println("Invalid Input!!.. Try Again\n");
+            }
+        }
     }
 
     // Support_Staff Module
