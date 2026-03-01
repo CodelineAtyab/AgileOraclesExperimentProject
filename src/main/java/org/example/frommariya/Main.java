91aae51d7c3d96229ba2a8a9fb3f34b288ebea79
Mariya-2001 <lamki01@hotmail.com>
2026-03-01 12:17:56 +0400

    Feature/86ewphr4h cli erp customer module create complaint (#67)
    * Worked in task 1-Project Setup and Main Menu
    
    * Worked in task 1-Project Setup and Main Menu - Resolved
    
    * Worked in task 2-Customer Module: Create Complaint
    
    * Worked in task 2-Customer Module: Create Complaint Resolved
    
    * Ignore target directory
    
    ---------
    
    Co-authored-by: Mariya <lamkia2001@gmail.com>

--- src/main/java/org/example/frommariya/Main.java
+++ src/main/java/org/example/frommariya/Main.java
@@ -1,13 +1,16 @@
 package org.example.frommariya;
 
 import java.util.Scanner;
+import java.util.ArrayList;
+
 
 
 public class Main {
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
-
+      
+        ArrayList<ComplaintC> complaintCS = new ArrayList<>();
 
         while (true){
             System.out.println("1. Customer");
@@ -19,6 +22,8 @@ public class Main {
             scanner.nextLine();
 
             if ( choice == 1 ){
+                CustomerModule.CustomertMenu(scanner,complaintCS);
+
                 System.out.println("Customer...");
             }
             else if ( choice == 2 ){
