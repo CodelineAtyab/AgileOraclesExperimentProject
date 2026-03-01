e57d0b9987bae2fcc893e89cd2f9103eb21d2102
TibyanSaad <saad.tibyan02@gmail.com>
2026-03-01 07:34:45 +0400

    Feature/86ewpgggy customer module create complaint (#64)
    * intial customer module setup
    
    * user input validation added for complaint ID but complaint priority input validation needs fixing
    
    * customer module layout, have to fix user input validation
    
    * complaint ID input validation added, complaint priority ID to fix
    
    * fixed all customer input validation

--- src/main/java/org/example/fromtibyan/CLIERP/Main.java
+++ src/main/java/org/example/fromtibyan/CLIERP/Main.java
@@ -11,7 +11,7 @@ public class Main {
             System.out.println("######### MAIN MENU #########");
             System.out.println("1.Customer \n2.Admin \n3.Support Staff \n4.Exit");
             System.out.println("#############################");
-            if (userInput.hasNextInt()){
+            if (userInput.hasNextInt()) {
                 int option = userInput.nextInt();
                 if (option == 1) {
                     //customer menu
@@ -25,7 +25,7 @@ public class Main {
                 } else if (option == 4) {
                     System.out.println("Exiting the application. Goodbye!");
                     running = false;
-                }else {
+                } else {
                     System.out.println("Please choose a number between 1-4");
                 }
             } else {
