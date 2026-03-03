4dc7bd83bf68a3b961cd64c603e020a99735be75
Ya'Rab Al-Mamari <yarop123456789@gmail.com>
2026-03-03 08:49:26 +0400

    some modifications

--- src/main/java/org/example/fromyarab/Main.java
+++ src/main/java/org/example/fromyarab/Main.java
@@ -38,7 +38,7 @@ public class Main{
                 Admin(complaints);
             }
             else if (choice == 3){
-                Support_Staff();
+                Support_Staff(complaints);
             }
             else if(choice == 4){
                 System.out.println("GoodBay!...");
@@ -155,8 +155,11 @@ public class Main{
                         System.out.println(counter + ". Complaint ID: " + complaint.get("ID"));
                         System.out.println("   Complaint Description: " + complaint.get("Description"));
                         System.out.println("   Complaint Priority: " + complaint.get("Priority"));
-                        System.out.println("   Complaint Assigned Staff: " + complaint.get("Staff"));
+                        System.out.println("   Complaint Assigned Support Staff: " + complaint.get("Staff"));
                         System.out.println("   Complaint Status: " + complaint.get("Status"));
+                        if (complaint.containsKey("Comment")) {
+                            System.out.println("   Compliant Comments: " + complaint.get("Comment"));
+                        }
                         System.out.println();
                         counter += 1;
                     }
@@ -191,8 +194,11 @@ public class Main{
                             System.out.println("Complaint ID: "+ searchedComplaint.get("ID"));
                             System.out.println("Complaint Description: " + searchedComplaint.get("Description"));
                             System.out.println("Complaint Priority: " + searchedComplaint.get("Priority"));
-                            System.out.println("Complaint Assigned Staff: " + searchedComplaint.get("Staff"));
+                            System.out.println("Complaint Assigned Support Staff: " + searchedComplaint.get("Staff"));
                             System.out.println("Complaint Status: " + searchedComplaint.get("Status"));
+                            if (searchedComplaint.containsKey("Comment")) {
+                                System.out.println("Compliant Comments: " + searchedComplaint.get("Comment"));
+                            }
                             System.out.println();
                         }
                         else {
@@ -281,9 +287,9 @@ public class Main{
                                 System.out.println("The Complaint Already Closed ...\n");
                             }
                             else {
-                                System.out.print("Enter Staff Name: ");
+                                System.out.print("Enter Support Staff Name: ");
                                 String staffName = scan.nextLine();
-                                assignComplaint.replace("Staff", staffName);
+                                assignComplaint.replace("Staff", staffName.toUpperCase());
                                 complaints.set(index, assignComplaint);
                                 System.out.printf("The Complaint (%d) Has Been Assigned Successfully TO (%s)>...\n\n" , ID , staffName);
                             }
@@ -305,8 +311,178 @@ public class Main{
     }
 
     // Support_Staff Module
-    static void Support_Staff(){
+    static void Support_Staff(ArrayList<Map<String, Object>> complaints){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String staffName = scan.nextLine();
+        staffName = staffName.toUpperCase();
 
+        boolean work = true;
+        while (work){
+            System.out.printf("   --- Support Staff %s Menu ---\n" , staffName);
+            System.out.println("1. View all assigned complaints");
+            System.out.println("2. Search complaint by ID");
+            System.out.println("3. Resolve Complaint");
+            System.out.println("4. Exit Support Staff menu");
+            System.out.print("\nEnter your choice: ");
+            String sChoice = scan.nextLine();
+            int choice = 0;
+
+            // check user input
+            if (isNumeric(sChoice)){
+                choice = Integer.parseInt(sChoice);
+            }
+            // handle user choice
+            // list staff assigned complaints
+            if (choice == 1){
+                int cout = 1;
+                Map<String, Object> staffComplaint = new HashMap<>();
+                for (Map complaint : complaints){
+                    if (complaint.get("Staff").equals(staffName)) {
+                        if (cout == 1){
+                            System.out.printf("List of assigned complaints to %s: \n" ,staffName);
+                            System.out.println(cout + ". Complaint ID: " + complaint.get("ID"));
+                            System.out.println("   Complaint Description: " + complaint.get("Description"));
+                            System.out.println("   Complaint Priority: " + complaint.get("Priority"));
+                            System.out.println("   Complaint Assigned Support Staff: " + complaint.get("Staff"));
+                            System.out.println("   Complaint Status: " + complaint.get("Status"));
+                            if (complaint.containsKey("Comment")) {
+                                System.out.println("   Compliant Comments: " + complaint.get("Comment"));
+                            }
+                            System.out.println();
+                            cout += 1;
+                        }
+                        else {
+                            System.out.println(cout + ". Complaint ID: " + complaint.get("ID"));
+                            System.out.println("   Complaint Description: " + complaint.get("Description"));
+                            System.out.println("   Complaint Priority: " + complaint.get("Priority"));
+                            System.out.println("   Complaint Assigned Support Staff: " + complaint.get("Staff"));
+                            System.out.println("   Complaint Status: " + complaint.get("Status"));
+                            if (complaint.containsKey("Comment")) {
+                                System.out.println("   Compliant Comments: " + complaint.get("Comment"));
+                            }
+                            System.out.println();
+                            cout += 1;
+                        }
+                    }
+                }
+                if (cout == 1){
+                    System.out.println("There are no assigned complaints to you ...");
+                }
+            }
+            else if (choice == 2){
+                // check if there are assigned complaints to staff
+                int cout = 0;
+                Map<String, Object> staffComplaint = new HashMap<>();
+                for (Map complaint : complaints){
+                    if (complaint.get("Staff").equals(staffName)) {
+                        cout += 1;
+                    }
+                }
+                if (cout == 0){
+                    System.out.println("There are no assigned complaints to you ...");
+                }
+                else {
+                    System.out.print("Enter Complaint ID: ");
+                    String sID = scan.nextLine();
+                    if (!isNumeric(sID)){
+                        System.out.println("Error! Please Numbers Are Only Allowed For Complaint ID ..");
+                        System.out.println("Returning to Staff Menu >>>\n");
+                    }
+                    else {
+                        // handle complaint id
+                        int ID = Integer.parseInt(sID);
+                        int found = 0;
+                        Map<Object, Object> searchedComplaint = new HashMap<>();
+                        for (Map complaint : complaints){
+                            if (complaint.get("ID").equals(ID) && complaint.get("Staff").equals(staffName)){
+                                searchedComplaint = complaint;
+                                found = 1;
+                            }
+                        }
+                        // print the complaint info
+                        if (found == 1){
+                            System.out.println(" -- Complaint Info --");
+                            System.out.println("Complaint ID: "+ searchedComplaint.get("ID"));
+                            System.out.println("Complaint Description: " + searchedComplaint.get("Description"));
+                            System.out.println("Complaint Priority: " + searchedComplaint.get("Priority"));
+                            System.out.println("Complaint Assigned Support Staff: " + searchedComplaint.get("Staff"));
+                            System.out.println("Complaint Status: " + searchedComplaint.get("Status"));
+                            if (searchedComplaint.containsKey("Comment")) {
+                                System.out.println("Compliant Comments: " + searchedComplaint.get("Comment"));
+                            }
+                            System.out.println();
+                        }
+                        else {
+                            System.out.printf("There is no complaint with ID number (%d) for you \n\n" ,ID);
+                        }
+                    }
+                }
+            }
+            else if (choice == 3){
+                // check if there are assigned complaints to staff
+                int cout = 0;
+                Map<String, Object> staffComplaint = new HashMap<>();
+                for (Map complaint : complaints){
+                    if (complaint.get("Staff").equals(staffName)) {
+                        cout += 1;
+                    }
+                }
+                if (cout == 0){
+                    System.out.println("There are no assigned complaints to you ...");
+                }
+                else {
+                    System.out.print("Enter Complaint ID to Resolve: ");
+                    String sID = scan.nextLine();
+                    if (!isNumeric(sID)){
+                        System.out.println("Error! Please Numbers Are Only Allowed For Complaint ID ..");
+                        System.out.println("Returning to Administrator Menu >>>\n");
+                    }
+                    else {
+                        // handle complaint id
+                        int ID = Integer.parseInt(sID);
+                        int found = 0;
+                        int index = 0;
+                        int couter = 0;
+                        Map<String, Object> resulvedComplaint = new HashMap<>();
+                        for (Map complaint : complaints){
+                            if (complaint.get("ID").equals(ID) && complaint.get("Staff").equals(staffName)) {
+                                resulvedComplaint = complaint;
+                                index = couter;
+                                found = 1;
+                            }
+                            couter = couter + 1;
+                        }
+
+                        // resolve the complaint and add comments
+                        if (found == 1){
+                            // check if the complaint status is closed before resolve it
+                            if (resulvedComplaint.get("Status").equals("CLOSED")){
+                                System.out.println("The Complaint Already Closed ...\n");
+                            }
+                            else {
+                                System.out.printf("Enter comments for this complaint(%d): " , ID);
+                                String comments = scan.nextLine();
+                                resulvedComplaint.put("Comment", comments);
+                                resulvedComplaint.replace("Status" , "CLOSED");
+                                complaints.set(index, resulvedComplaint);
+                                System.out.printf("The Complaint (%d) Has Been Resolved Successfully >...\n\n" , ID);
+                            }
+                        }
+                        else {
+                            System.out.printf("There is no complaint with ID number (%d) \n\n" ,ID);
+                        }
+                    }
+                }
+            }
+            else if (choice == 4){
+                System.out.println("Exiting Staff menu!...\n");
+                work = false;
+            }
+            else {
+                System.out.println("Invalid Input!!.. Try Again\n");
+            }
+        }
     }
 
     // Function to check if the string is integer
