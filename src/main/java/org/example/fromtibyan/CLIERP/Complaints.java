693a827848d1c2abddf4214701a1f89682eb05dd
TibyanSaad <saad.tibyan02@gmail.com>
2026-03-04 17:14:30 +0400

    admin module extension added (#110)

--- src/main/java/org/example/fromtibyan/CLIERP/Complaints.java
+++ src/main/java/org/example/fromtibyan/CLIERP/Complaints.java
@@ -6,16 +6,15 @@ public class Complaints {
     String complaintPriority;
     String complaintStatus;
     String assignedStaff;
-    String staffComments;
 
 
-    public Complaints(int complaintId, String complaintDescription, String complaintPriority, String complaintStatus, String assignedStaff, String staffComments) {
+
+    public Complaints(int complaintId, String complaintDescription, String complaintPriority, String complaintStatus, String assignedStaff) {
         this.complaintId = complaintId;
         this.complaintDescription = complaintDescription;
         this.complaintPriority = complaintPriority;
         this.complaintStatus = complaintStatus;
         this.assignedStaff = assignedStaff;
-        this.staffComments = staffComments;
     }
 
     public int getComplaintId() {
@@ -38,7 +37,6 @@ public class Complaints {
         return assignedStaff;
     }
 
-
     public void setComplaintStatus(String complaintStatus){
         this.complaintStatus = complaintStatus;
     }
@@ -46,7 +44,5 @@ public class Complaints {
     public void setAssignStaff(String assignedStaff) {
         this.assignedStaff = assignedStaff;
     }
-
-
 
 }
