94013a7a043a86d217b10a6a328a7e01e9c09ecc
abdulmajeedalbalushi <codeline@example.com>
2026-03-08 12:58:53 +0400

    assigning to staff

--- src/main/java/org/example/formabdulmajeed/Main.java
+++ src/main/java/org/example/formabdulmajeed/Main.java
@@ -68,7 +68,7 @@ public class Main {
                 int pinAdmin = input.nextInt();
                 if (pinAdmin== 9266){
                     System.out.println("Correct PIN. You are logged in as admin");
-                    System.out.println("1. View complaints\n2. Search complaint\n3. close complaint\n4. Exit");
+                    System.out.println("1. View complaints\n2. Search complaint\n3. close complaint\n4. assign complin to staff\n5. view assigned compliant\n6. Exit");
                     int admainChoice = input.nextInt();
                     if (admainChoice == 1){
                         System.out.println("View all complaints: ");
@@ -114,8 +114,49 @@ public class Main {
                             complainStatus.set(closeIndex, "CLOSED");
                             System.out.println("Complaint closed successfully.");
                         }
+                    }
+                    else if (admainChoice == 4){
+                            System.out.println("Enter the compliant ID to assign to staff");
+                            int searchInId = input.nextInt();
+                            input.nextLine();
 
-                    }else if (userChoise== 4){
+                            int indexId = -1;
+
+                            for (int i = 0; i < complainId.size(); i++) {
+                                if (complainId.get(i) == searchInId) {
+                                    indexId = i;
+                                    break;
+                                }
+                            }
+
+                            if (indexId == -1) {
+                                System.out.println("Complaint not found.");
+                            }
+                            else if (complainStatus.get(indexId).equalsIgnoreCase("closed")) {
+                                System.out.println("Closed complaints cannot be assigned.");
+                            }
+                            else {
+                                System.out.println("Enter staff name:");
+                                String staff = input.nextLine();
+                                complainStaff.set(indexId, staff);
+                                System.out.println("Complaint assigned to " + staff);
+                            }
+                        }
+                        else if (admainChoice == 5){
+                            System.out.println("view all assigned compliant");
+                            if (complainId.isEmpty()){
+                                System.out.println("There is no compliants");
+                            }else{
+                                for (int i = 0; i < complainId.size(); i++){
+                                    System.out.println("ID:"+ complainId.get(i));
+                                    System.out.println("Descrption:"+ complainDescrption.get(i));
+                                    System.out.println("Priority:"+ complainPriority.get(i));
+                                    System.out.println("Status:"+ complainStatus.get(i));
+                                    System.out.println("Assigned Staff:"+ complainStaff.get(i));
+                                }
+                            }
+                        }
+                    else if (userChoise== 6){
                         System.out.println("Exit to main menu");
                     }
                 }
@@ -126,49 +167,9 @@ public class Main {
             }
             //=====staff
                 else if (userChoise == 3){
-                System.out.println("Support staff: \n1. assign complin to staff\n2. view assigned compliant");
-                int userStaff = input.nextInt();
-                if (userStaff == 1){
-                    System.out.println("Enter the compliant ID to assign to staff");
-                    int searchInId = input.nextInt();
-                    input.nextLine();
+                System.out.println("Support staff: \n1. \n2. ");
 
-                    int indexId = -1;
 
-                    for (int i = 0; i < complainId.size(); i++) {
-                        if (complainId.get(i) == searchInId) {
-                            indexId = i;
-                            break;
-                        }
-                    }
-
-                    if (indexId == -1) {
-                        System.out.println("Complaint not found.");
-                    }
-                    else if (complainStatus.get(indexId).equalsIgnoreCase("closed")) {
-                        System.out.println("Closed complaints cannot be assigned.");
-                    }
-                    else {
-                        System.out.println("Enter staff name:");
-                        String staff = input.nextLine();
-                        complainStaff.set(indexId, staff);
-                        System.out.println("Complaint assigned to " + staff);
-                    }
-                }
-                else if (userStaff == 2){
-                    System.out.println("view all assigned compliant");
-                    if (complainId.isEmpty()){
-                        System.out.println("There is no compliants");
-                    }else{
-                        for (int i = 0; i < complainId.size(); i++){
-                            System.out.println("ID:"+ complainId.get(i));
-                            System.out.println("Descrption:"+ complainDescrption.get(i));
-                            System.out.println("Priority:"+ complainPriority.get(i));
-                            System.out.println("Status:"+ complainStatus.get(i));
-                            System.out.println("Assigned Staff:"+ complainStaff.get(i));
-                        }
-                    }
-                }
 
             }
             else if (userChoise == 4){
