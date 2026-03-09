de7f31b0bd18efed8f76f80cfd3417605d061fb8
safaalmaamari0-source <you@example.com>
2026-03-09 08:25:40 +0400

    The Staircase Challenge

---
+++ src/main/java/org/example/fromsafaalmaamari/Sprint3Safaalmaamari/Staircase.java
@@ -0,0 +1,12 @@
+package org.example.fromsafaalmaamari.Sprint3Safaalmaamari;
+public class Staircase {
+    public static void main(String[] args){
+        for (int i = 1; i <= 5; i++){
+            for (int k = 1; k <= i; k++){
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+
+    }
+}
