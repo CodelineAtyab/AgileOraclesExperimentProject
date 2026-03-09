ecdcd2fa9fc85fd056a3e33e128ac855a66aaa9f
Ya'Rab Al-Mamari <yarop123456789@gmail.com>
2026-03-09 11:49:55 +0400

    use nested loop

--- src/main/java/org/example/fromyarab/PyramidOfNumbers.java
+++ src/main/java/org/example/fromyarab/PyramidOfNumbers.java
@@ -3,10 +3,11 @@ package org.example.fromyarab;
 public class PyramidOfNumbers {
     public static void main(String[] args){
         int rows = 5;
-        String print = "";
-
         for (int i=1 ; i<=rows ; i++){
-            print = "1".repeat(i);
+            String print = "";
+            for (int j=0 ; j<i ; j++){
+                print += "1";
+            }
             System.out.println(Integer.parseInt(print) * Integer.parseInt(print));
         }
     }
