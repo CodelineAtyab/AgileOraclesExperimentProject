3208fbf85b248ec6619bff72cd905c8beda845e7
AbdullahAlHadabi <Abdullah.Juma280@hotmail.com>
2026-03-10 12:15:25 +0400

    fix bugs

--- src/main/java/org/example/fromabdullahalhadabi/PyramidofNumbersLoopChallenge.java
+++ src/main/java/org/example/fromabdullahalhadabi/PyramidofNumbersLoopChallenge.java
@@ -1,4 +1,26 @@
 package org.example.fromabdullahalhadabi;
 
 public class PyramidofNumbersLoopChallenge {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 4; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+
+            }
+            for (int j = i - 1 ; j >= 1; j--) {
+                System.out.print(j);
+            }
+
+
+            System.out.println();
+
+
+
+
+
+
+        }
+    }
 }
