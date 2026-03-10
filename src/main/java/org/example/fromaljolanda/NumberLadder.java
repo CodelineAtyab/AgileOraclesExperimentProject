d241c04fa96b88b9c8cf094880521a6e678305e2
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-03-10 11:17:32 +0400

    Number Ladder: The Climbing Count

--- src/main/java/org/example/fromaljolanda/NumberLadder.java
+++ src/main/java/org/example/fromaljolanda/NumberLadder.java
@@ -13,7 +13,6 @@ public class NumberLadder {
             for (int j = 1; j <= i; j++) {
                 System.out.print(j + "\t");
             }
-
             System.out.println();
         }
     }
