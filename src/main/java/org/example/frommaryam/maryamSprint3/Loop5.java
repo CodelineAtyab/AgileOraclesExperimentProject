201bfff09b940ca921d978f8e36e89e56a97fe2e
MaryamGit˚🍮🥄 ˚₊‧ <maryamofficially24@gmail.com>
2026-03-11 21:55:03 +0400

    Loop 5

---
+++ src/main/java/org/example/frommaryam/maryamSprint3/Loop5.java
@@ -0,0 +1,27 @@
+package org.example.frommaryam.maryamSprint3;
+
+public class Loop5 {
+    public static void main(String[] args) {
+
+        int l=5;
+        int g=5;
+
+        for (int i=1; i<=5 ; i++){
+            for (int  j=1 ; j<=9 ; j++){
+                if(j == l || j == g || i==5){
+                    System.out.print("*");
+                }
+
+                else {
+                    System.out.print(" ");
+
+                    }
+            }
+
+            l=l-1;
+            g=g+1;
+            System.out.println(" ");
+
+        }
+    }
+}
