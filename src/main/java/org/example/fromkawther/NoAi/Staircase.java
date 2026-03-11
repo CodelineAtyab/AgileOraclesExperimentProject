382f14305836f8df15fd55981a7262c9a5de2ea7
kawther <likawther17@gmail.com>
2026-03-11 10:53:33 +0400

    Staircase

---
+++ src/main/java/org/example/fromkawther/NoAi/Staircase.java
@@ -0,0 +1,14 @@
+package org.example.fromkawther.NoAi;
+
+public class Staircase {
+    public static void main (String[] args) {
+        int lines = 5;
+        for (int i = 1; i <= lines; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("#");
+
+            }
+            System.out.println();
+        }
+
+    }}
