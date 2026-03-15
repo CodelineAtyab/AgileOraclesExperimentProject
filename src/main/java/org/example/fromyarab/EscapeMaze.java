e327a09ebf1e100ea8985c45055264fd3e4510b3
Ya'Rab Al-Mamari <yarop123456789@gmail.com>
2026-03-15 11:22:38 +0400

    Stack-Based Backtracking

--- src/main/java/org/example/fromyarab/EscapeMaze.java
+++ src/main/java/org/example/fromyarab/EscapeMaze.java
@@ -23,33 +23,36 @@ public class EscapeMaze {
             // check the validity of the maze data
             int enPointCounter = 0;
             int exPointCounter = 0;
-            int[] enPointCoordinate = new int[2];
-            int[] exPointCoordinate = new int[2];
+            ArrayList<Integer> enPointCoordinate = new ArrayList<>();
+            ArrayList<Integer> exPointCoordinate = new ArrayList<>();
             for (int i=0; i<maze.size(); i++){
                 for (int j=0; j<maze.get(i).size(); j++){
                     // check the characters
                     if (!maze.get(i).get(j).equals("1") && !maze.get(i).get(j).equals("0") && !maze.get(i).get(j).equals("@") && !maze.get(i).get(j).equals("E")){
                         System.out.println("Maze Not Valid!! -> Characters restriction violation...");
+                        return;
                     }
 
                     // check corners of the maze
                     if ((i==0 || i==maze.size()-1) && (j==0 || j==maze.get(i).size()-1) && (maze.get(i).get(j).equals("@") || maze.get(i).get(j).equals("E"))){
                         System.out.printf("Maze Not Valid!! -> Border restriction violation...(Corner: <i>%d <j>%d)\n",i,j);
+                        return;
                     }
                     // check borders
                     if (i==0 || j==0 || i==maze.size()-1 || j==maze.get(i).size()-1){
                         if (maze.get(i).get(j).equals("@")){
                             enPointCounter += 1;
-                            enPointCoordinate[0] = i;
-                            enPointCoordinate[1] = j;
+                            enPointCoordinate.add(i);
+                            enPointCoordinate.add(j);
                         }
                         else if (maze.get(i).get(j).equals("E")){
                             exPointCounter += 1;
-                            exPointCoordinate[0] = i;
-                            exPointCoordinate[1] = j;
+                            exPointCoordinate.add(i);
+                            exPointCoordinate.add(j);
                         }
                         else if (!maze.get(i).get(j).equals("1")){
                             System.out.println("Maze Not Valid!! -> Border restriction violation...");
+                            return;
                         }
                     }
                 }
@@ -57,71 +60,97 @@ public class EscapeMaze {
             // check number of entry and exit points
             if (enPointCounter != 1 || exPointCounter != 1){
                 System.out.println("Maze Not Valid!! -> # Entry/Exit point violation...");
+                return;
             }
 
             // check coordinates of entry and exit points
-            if (enPointCoordinate[0] == exPointCoordinate[0] || enPointCoordinate[1] == exPointCoordinate[1]) {
+            if (enPointCoordinate.get(0).equals(exPointCoordinate.get(0)) || enPointCoordinate.get(1).equals(exPointCoordinate.get(1))) {
                 System.out.println("Maze Not Valid!! -> Entry/Exit point coordinate violation...");
+                return;
             }
 
-            //===================================
+            //==================================================================
+            //==================================================================
             // Backtracking
-            Stack<int[]> backTrack = new Stack<>();
+            Stack<ArrayList<Integer>> backTrack = new Stack<>();
             int[] currentPosition = new int[2];
-            int[] nextPosition = new int[2];
-            ArrayList<int[]> visited = new ArrayList<>();
+            ArrayList<ArrayList<Integer>> visited = new ArrayList<>();
             visited.add(enPointCoordinate);
-            currentPosition = enPointCoordinate;
+            currentPosition[0] = enPointCoordinate.get(0);
+            currentPosition[1] = enPointCoordinate.get(1);
             boolean exit = false;
             while (!exit){
-                if (maze.get(currentPosition[0]).get(currentPosition[1]).equals("E") || backTrack.isEmpty()){
-                    exit = true;
-                }
+                System.out.println("--------------");
+                System.out.println("Current Position: "+ Arrays.toString(currentPosition));
 
                 if (currentPosition[0] == 0){
                     if (maze.get(currentPosition[0]+1).get(currentPosition[1]).equals("0")){
-
+                        System.out.println("Move Down");
+                        backTrack.push(new ArrayList<>(List.of(currentPosition[0],currentPosition[1])));
+                        visited.add(new ArrayList<>(List.of(currentPosition[0],currentPosition[1])));
+                        currentPosition[0] = currentPosition[0]+1;
                     }
                 }
                 else if (currentPosition[0] == maze.size()-1){
                     if (maze.get(currentPosition[0]-1).get(currentPosition[1]).equals("0")){
-
+                        System.out.println("Move Up");
+                        backTrack.push(new ArrayList<>(List.of(currentPosition[0],currentPosition[1])));
+                        visited.add(new ArrayList<>(List.of(currentPosition[0],currentPosition[1])));
+                        currentPosition[0] = currentPosition[0]-1;
                     }
                 }
                 else if (currentPosition[1] == 0){
                     if (maze.get(currentPosition[0]).get(currentPosition[1]+1).equals("0")){
+                        System.out.println("Move Right");
+                        backTrack.push(new ArrayList<>(List.of(currentPosition[0],currentPosition[1])));
+                        visited.add(new ArrayList<>(List.of(currentPosition[0],currentPosition[1])));
                         currentPosition[1] = currentPosition[1]+1;
                     }
                 }
                 else if (currentPosition[1] == maze.get(1).size()-1){
                     if (maze.get(currentPosition[0]).get(currentPosition[1]-1).equals("0")){
-
+                        System.out.println("Move Left");
+                        backTrack.push(new ArrayList<>(List.of(currentPosition[0],currentPosition[1])));
+                        visited.add(new ArrayList<>(List.of(currentPosition[0],currentPosition[1])));
+                        currentPosition[1] = currentPosition[1]-1;
                     }
                 }
                 else {
-                    if (!visited.contains(List.of(currentPosition[0]+1,currentPosition[1])) && maze.get(currentPosition[0]+1).get(currentPosition[1]).equals("0")){
-                        backTrack.push(currentPosition);
-                        visited.add(currentPosition);
+                    if (!visited.contains(List.of(currentPosition[0]+1,currentPosition[1])) && (maze.get(currentPosition[0]+1).get(currentPosition[1]).equals("0") || maze.get(currentPosition[0]+1).get(currentPosition[1]).equals("E"))){
+                        System.out.println("Move Down");
+                        backTrack.push(new ArrayList<>(List.of(currentPosition[0],currentPosition[1])));
+                        visited.add(new ArrayList<>(List.of(currentPosition[0],currentPosition[1])));
                         currentPosition[0] = currentPosition[0]+1;
                     }
-                    else if (!visited.contains(List.of(currentPosition[0],currentPosition[1]+1)) && maze.get(currentPosition[0]).get(currentPosition[1]+1).equals("0")){
-                        backTrack.push(currentPosition);
-                        visited.add(currentPosition);
-                        currentPosition[1] = currentPosition[0]+1;
+                    else if (!visited.contains(List.of(currentPosition[0],currentPosition[1]+1)) && (maze.get(currentPosition[0]).get(currentPosition[1]+1).equals("0") || maze.get(currentPosition[0]).get(currentPosition[1]+1).equals("E"))){
+                        System.out.println("Move Right");
+                        backTrack.push(new ArrayList<>(List.of(currentPosition[0],currentPosition[1])));
+                        visited.add(new ArrayList<>(List.of(currentPosition[0],currentPosition[1])));
+                        currentPosition[1] = currentPosition[1]+1;
                     }
-                    else if (!visited.contains(List.of(currentPosition[0]-1,currentPosition[1])) && maze.get(currentPosition[0]-1).get(currentPosition[1]).equals("0")){
-                        backTrack.push(currentPosition);
-                        visited.add(currentPosition);
+                    else if (!visited.contains(List.of(currentPosition[0]-1,currentPosition[1])) && (maze.get(currentPosition[0]-1).get(currentPosition[1]).equals("0") || maze.get(currentPosition[0]-1).get(currentPosition[1]).equals("E"))){
+                        System.out.println("Move Up");
+                        backTrack.push(new ArrayList<>(List.of(currentPosition[0],currentPosition[1])));
+                        visited.add(new ArrayList<>(List.of(currentPosition[0],currentPosition[1])));
                         currentPosition[0] = currentPosition[0]-1;
                     }
-                    else if (!visited.contains(List.of(currentPosition[0],currentPosition[1]-1)) && maze.get(currentPosition[0]).get(currentPosition[1]-1).equals("0")){
-                        backTrack.push(currentPosition);
-                        visited.add(currentPosition);
-                        currentPosition[1] = currentPosition[0]-1;
+                    else if (!visited.contains(List.of(currentPosition[0],currentPosition[1]-1)) && (maze.get(currentPosition[0]).get(currentPosition[1]-1).equals("0") || maze.get(currentPosition[0]).get(currentPosition[1]-1).equals("E"))){
+                        System.out.println("Move Left");
+                        backTrack.push(new ArrayList<>(List.of(currentPosition[0],currentPosition[1])));
+                        visited.add(new ArrayList<>(List.of(currentPosition[0],currentPosition[1])));
+                        currentPosition[1] = currentPosition[1]-1;
                     }
                     else{
-                        currentPosition = backTrack.pop();
+                        System.out.println("Pop");
+                        ArrayList<Integer> next = backTrack.pop();
+                        visited.add(new ArrayList<>(List.of(currentPosition[0],currentPosition[1])));
+                        currentPosition[0] = next.get(0);
+                        currentPosition[1] = next.get(1);
                     }
+                }
+
+                if (maze.get(currentPosition[0]).get(currentPosition[1]).equals("E") || backTrack.isEmpty()){
+                    exit = true;
                 }
             }
             if (maze.get(currentPosition[0]).get(currentPosition[1]).equals("E")){
