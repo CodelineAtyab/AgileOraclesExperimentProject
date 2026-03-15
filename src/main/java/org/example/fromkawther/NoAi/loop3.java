464bba213611fa3ec83435790ade6370b0f4dbbf
kawther <likawther17@gmail.com>
2026-03-15 21:34:20 +0400

    add new loop3

---
+++ src/main/java/org/example/fromkawther/NoAi/loop3.java
@@ -0,0 +1,17 @@
+package org.example.fromkawther.NoAi;
+
+public class loop3 {
+    public static void main (String[] args) {
+
+        int lines = 5;
+        for (int i = 1; i <= lines; i++) {
+            for (int j = i; j < lines; j++) {
+                System.out.print("");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }}
+
