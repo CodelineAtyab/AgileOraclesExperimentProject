6e7b2aee30fb6ae8b681c0a68180c8c84f5d2750
kawther <likawther17@gmail.com>
2026-03-16 11:59:36 +0400

    implemented start pyramid

---
+++ src/main/java/org/example/fromkawther/NoAi/Advanced.java
@@ -0,0 +1,27 @@
+package org.example.fromkawther.NoAi;
+
+public class Advanced {
+    public static void main(String[] args) {
+
+        int lines = 5;
+
+        for (int i = 1; i <= lines; i++) {
+
+            for (int s = i; s < lines; s++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                if (k == 1 || k == (2 * i - 1) || i == lines) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+
