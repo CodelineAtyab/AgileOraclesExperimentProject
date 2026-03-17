ad4e5413b89e6cfa63db731979a333c6ebb227a5
kawther <likawther17@gmail.com>
2026-03-17 23:33:31 +0400

    fixed the file that i uplode it

---
+++ src/main/java/org/example/fromkawther/erp/List2.java
@@ -0,0 +1,57 @@
+package org.example.fromkawther.erp;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+class Complaint {
+    int id;
+    String description;
+    String priority;
+
+    Complaint(int id, String description, String priority) {
+        this.id = id;
+        this.description = description;
+        this.priority = priority;
+    }
+}
+
+public class List2{
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Complaint> list = new ArrayList<>();
+
+        int id;
+        String description;
+        String priority = "MEDIUM";
+
+        System.out.print("Enter Complaint ID: ");
+        if (sc.hasNextInt()) {
+            id = sc.nextInt();
+            sc.nextLine();
+        } else {
+            System.out.println("Invalid ID");
+            return;
+        }
+
+        System.out.print("Enter the Discraption about lists: ");
+        description = sc.nextLine();
+
+        System.out.print("Enter Priority (low /Medium / High): ");
+        String input = sc.nextLine();
+
+        if (input.equalsIgnoreCase("low") ||
+         input.equalsIgnoreCase("Medium") ||
+         input.equalsIgnoreCase(" High")) {
+      priority = input.toUpperCase();
+        }
+
+    list.add(new Complaint(id, description, priority));
+
+  System.out.println("Complaint added successfully!");
+
+        sc.close();
+    }
+}
+
