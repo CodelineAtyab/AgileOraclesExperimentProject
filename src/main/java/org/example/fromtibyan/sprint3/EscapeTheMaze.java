dcffd387ce78b4f9c0837aed65d64ff0a7d6d208
TibyanSaad <saad.tibyan02@gmail.com>
2026-03-18 13:26:05 +0400

    fixed border and one symbol function validation

--- src/main/java/org/example/fromtibyan/sprint3/EscapeTheMaze.java
+++ src/main/java/org/example/fromtibyan/sprint3/EscapeTheMaze.java
@@ -1,4 +1,5 @@
 package org.example.fromtibyan.sprint3;
+
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.*;
@@ -15,26 +16,26 @@ public class EscapeTheMaze {
             for (int i = 0; i < mazeArray.size(); i++) {
                 mazeArray2D[i] = mazeArray.get(i).toCharArray();
             }
-            if (onlyOneSymbol(mazeArray2D, mazeArray) && allBordersOnes(mazeArray2D, mazeArray)){
+            if (onlyOneSymbol(mazeArray2D, mazeArray) && allBordersOnes(mazeArray2D, mazeArray)) {
                 int startRow = 0;
                 int startColumn = 0;
-                for (int row = 0; row<mazeArray.size();row++ ){
-                    for(int column = 0 ; column<mazeArray.get(0).length(); column++){
+                for (int row = 0; row < mazeArray.size(); row++) {
+                    for (int column = 0; column < mazeArray.get(0).length(); column++) {
                         if (mazeArray2D[row][column] == '@') {
                             startRow = row;
                             startColumn = column;
                         }
                     }
                 }
-                Position start = new Position(startRow,startColumn);
+                Position start = new Position(startRow, startColumn);
 
-                if (symbolMovement(mazeArray2D,start, mazeArray)){
+                if (symbolMovement(mazeArray2D, start, mazeArray)) {
                     System.out.println("Maze Solved!");
-                }else{
+                } else {
                     System.out.println("No path found.");
                 }
 
-            }else {
+            } else {
                 System.out.println("Border conditions are not met.");
             }
 
@@ -42,7 +43,6 @@ public class EscapeTheMaze {
         } catch (IOException e) {
             System.err.println("Error reading the maze file: " + e.getMessage()); // misreading the file
         }
-
 
 
     }
@@ -54,8 +54,8 @@ public class EscapeTheMaze {
 
         int endRow = 0;
         int endColumn = 0;
-        for (int row = 0; row<mazeArray.size();row++ ){
-            for(int column = 0 ; column<mazeArray.get(0).length(); column++){
+        for (int row = 0; row < mazeArray.size(); row++) {
+            for (int column = 0; column < mazeArray.get(0).length(); column++) {
                 if (mazeArray2D[row][column] == 'E') {
                     endRow = row;
                     endColumn = column;
@@ -63,32 +63,32 @@ public class EscapeTheMaze {
             }
         }
 
-        while (!mazeStack.empty()){
-            Position current = mazeStack.pop();
+        while (!mazeStack.empty()) {
+            Position current = mazeStack.pop(); // pop top variable for current position
             int r = current.row;
             int c = current.col;
 
 
             // move @ between the border & wall
-            if (r<0 || c<0||r>=mazeArray.size()||c>=mazeArray.get(0).length()|| mazeArray2D[r][c] == '1' || visitedCell[r][c]){
+            if (r < 0 || c < 0 || r >= mazeArray.size() || c >= mazeArray.get(0).length() || mazeArray2D[r][c] == '1' || visitedCell[r][c]) {
                 continue;
             }
-            visitedCell[r][c]= true; // to not visit again
+            visitedCell[r][c] = true; // to not visit again
             mazeArray2D[r][c] = '@';
-            for(char[] maze: mazeArray2D){
+            for (char[] maze : mazeArray2D) {
                 System.out.println(maze);
             }
 
-            for (int space = 0; space<4; space ++){
-                System.out.println( );
+            for (int space = 0; space < 4; space++) {
+                System.out.println();
             }
 
-            mazeStack.push(new Position(r+1,c));//down
-            mazeStack.push(new Position(r-1,c));//up
-            mazeStack.push(new Position(r,c+1));//right
-            mazeStack.push(new Position(r,c-1));//left
+            mazeStack.push(new Position(r + 1, c));//down
+            mazeStack.push(new Position(r - 1, c));//up
+            mazeStack.push(new Position(r, c + 1));//right
+            mazeStack.push(new Position(r, c - 1));//left
             mazeArray2D[r][c] = '0';
-            if (r == endRow && c == endColumn){
+            if (r == endRow && c == endColumn) {
                 return true;
             }
             Thread.sleep(1000);
@@ -97,51 +97,52 @@ public class EscapeTheMaze {
     }
 
 
-
-
-    public static boolean onlyOneSymbol(char[][] mazeArray2D, ArrayList<String> mazeArray){
+    public static boolean onlyOneSymbol(char[][] mazeArray2D, ArrayList<String> mazeArray) {
         //checking if there is one @ and one E
         int atCounter = 0;
         int eCounter = 0;
 
-        for (int row = 0; row<mazeArray.size();row++ ){
-            for(int column = 0 ; column<mazeArray.get(0).length(); column++){
+        for (int row = 0; row < mazeArray.size(); row++) {
+            for (int column = 0; column < mazeArray.get(0).length(); column++) {
                 if (mazeArray2D[row][column] == '@') {
                     atCounter++;
-                    if (atCounter >1){
+                    if (atCounter > 1) {
                         return false;
                     }
                 }
                 if (mazeArray2D[row][column] == 'E') {
                     eCounter++;
-                    if (eCounter >1){
+                    if (eCounter > 1) {
                         return false;
                     }
                 }
             }
         }
-        return true;
+        return atCounter == 1 && eCounter == 1; // only when there is one symbol of each and not 0
     }
 
-    public static boolean allBordersOnes(char[][] mazeArray2D, ArrayList<String> mazeArray){
+    public static boolean allBordersOnes(char[][] mazeArray2D, ArrayList<String> mazeArray) {
         // maze borders condition of all 1s
         int index ;
-        for (index = 0; index < mazeArray.size(); index++) {
+        for (index = 0; index < 10; index++) {
             char columnCellFirst = mazeArray2D[0][index];
-            char columnCellLast = mazeArray2D[mazeArray.size()-1][index];
+            char columnCellLast = mazeArray2D[9][index];
             char rowCellFirst = mazeArray2D[index][0];
-            char rowCellLast = mazeArray2D[index][mazeArray.get(0).length()-1];
-            if (columnCellFirst == '1' || columnCellFirst == '@' || columnCellFirst == 'E') {
-                if (columnCellLast == '1' || columnCellLast == '@' || columnCellLast == 'E') {
-                    if (rowCellFirst == '1' || rowCellFirst == '@' || rowCellFirst == 'E') {
-                        if (rowCellLast == '1' || rowCellLast == '@' || rowCellLast == 'E') {
-                            return true;
-                        }
-                    }
-                }
-
+            char rowCellLast = mazeArray2D[index][9];
+            if (!(columnCellFirst == '1' || columnCellFirst == '@' || columnCellFirst == 'E')) {
+                return false;
             }
-        }return false;
+            if (!(columnCellLast == '1' || columnCellLast == '@' || columnCellLast == 'E')) {
+                return false;
+            }
+            if (!(rowCellFirst == '1' || rowCellFirst == '@' || rowCellFirst == 'E')) {
+                return false;
+            }
+            if (!(rowCellLast == '1' || rowCellLast == '@' || rowCellLast == 'E')) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
