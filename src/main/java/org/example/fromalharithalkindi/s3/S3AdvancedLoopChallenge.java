fbc139738a6dd896b0527502bf6c1e321f0ac36c
alharith alkindi <alharithalk>
2026-03-24 23:53:05 +0400

    task 86ewwr3y0 solved

---
+++ src/main/java/org/example/fromalharithalkindi/s3/S3AdvancedLoopChallenge.java
@@ -0,0 +1,23 @@
+package org.example.fromalharithalkindi.s3;
+
+public class S3AdvancedLoopChallenge {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+
+            for (int j = 1; j <= 5 - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1) || i == 5) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
