941911d6c358deb644099b25de45600007be5835
kawther <likawther17@gmail.com>
2026-03-26 13:45:25 +0400

    fix the list 2 task

--- src/main/java/org/example/fromkawther/erp/List2.java
+++ src/main/java/org/example/fromkawther/erp/List2.java
@@ -1,57 +1,51 @@
 package org.example.fromkawther.erp;
+
 import java.util.Scanner;
-import java.util.ArrayList;
 
 
-class Complaint {
-    int id;
-    String description;
-    String priority;
-
-    Complaint(int id, String description, String priority) {
-        this.id = id;
-        this.description = description;
-        this.priority = priority;
-    }
-}
-
-public class List2{
+public class List2 {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        ArrayList<Complaint> list = new ArrayList<>();
+        int[] complaintID = new int[100];
+        String[] description = new String[100];
+        String[] priority = new String[100];
 
-        int id;
-        String description;
-        String priority = "MEDIUM";
+        int count = 0;
 
-        System.out.print("Enter Complaint ID: ");
-        if (sc.hasNextInt()) {
-            id = sc.nextInt();
-            sc.nextLine();
-        } else {
-            System.out.println("Invalid ID");
+        System.out.print("Enter Complaint ID (numeric): ");
+        if (!sc.hasNextInt()) {
+            System.out.println("Invalid input! ID must be numeric.");
             return;
         }
+        complaintID[count] = sc.nextInt();
+        sc.nextLine();
 
-        System.out.print("Enter the Discraption about lists: ");
-        description = sc.nextLine();
+        System.out.print("Enter Complaint Description: ");
+        description[count] = sc.nextLine();
 
-        System.out.print("Enter Priority (low /Medium / High): ");
-        String input = sc.nextLine();
 
-        if (input.equalsIgnoreCase("low") ||
-         input.equalsIgnoreCase("Medium") ||
-         input.equalsIgnoreCase(" High")) {
-      priority = input.toUpperCase();
+        System.out.print("Enter Priority (LOW / MEDIUM (default) / HIGH): ");
+        String priorityInput = sc.nextLine();
+
+        if (priorityInput.equals("")) {
+            priority[count] = "MEDIUM";
+        } else if (priorityInput.equals("LOW") || priorityInput.equals("MEDIUM") || priorityInput.equals("HIGH")) {
+            priority[count] = priorityInput;
+        } else {
+            System.out.println("Invalid priority! MEDIUM priority set.");
+            priority[count] = "MEDIUM";
         }
 
-    list.add(new Complaint(id, description, priority));
+        count++;
 
-  System.out.println("Complaint added successfully!");
+        System.out.println("\nComplaint Created Successfully!");
+        System.out.println("ID: " + complaintID[count - 1]);
+        System.out.println("Description: " + description[count - 1]);
+        System.out.println("Priority: " + priority[count - 1]);
 
         sc.close();
+
     }
 }
-
