35eaac25a66e55e89bc8a5a1b74be3d28a84e2a6
Syed Atyab Hussain <syed.atyab.hussain.om@gmail.com>
2026-03-29 16:01:12 +0400

    Added player location fetch function

--- src/main/java/org/example/fromatyab/FileManager.java
+++ src/main/java/org/example/fromatyab/FileManager.java
@@ -8,7 +8,9 @@ import java.nio.file.Path;
 public class FileManager {
     public static void main(String[] args) {
         char[][] maze = loadAndGetMaze();
-        // int[] initialPlayerPosition = getPLayerLocation(maze);
+        int[] initialPlayerPosition = getPLayerLocation(maze);
+
+        System.out.printf("Location of @ is (%d,%d)\n", initialPlayerPosition[0] + 1, initialPlayerPosition[1] + 1);
 
         System.out.println("Before the change:");
         displayMaze(maze);
@@ -60,11 +62,18 @@ public class FileManager {
         return maze;
     }
 
-//    public static int[] getPLayerLocation(char[][] maze) {
-//        for (int row=0; row<maze.length; row++) {
-//
-//        }
-//    }
+    public static int[] getPLayerLocation(char[][] maze) {
+        int[] location = new int[2];
+        for (int row=0; row < maze.length; row++) {
+            for (int col=0; col < maze[row].length; col++) {
+                if (maze[row][col] == '@') {
+                    location[0] = row;
+                    location[1] = col;
+                }
+            }
+        }
+        return location;
+    }
 
     public static final String relativeMazePath = "./data/maze.txt";
 }
