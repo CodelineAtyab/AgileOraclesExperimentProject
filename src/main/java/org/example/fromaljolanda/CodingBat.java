a66dc5b86bb5df9c270121145366066e53b8c1b2
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-03-29 10:10:49 +0400

    CodingBat Practice modifay

--- src/main/java/org/example/fromaljolanda/CodingBat.java
+++ src/main/java/org/example/fromaljolanda/CodingBat.java
@@ -1,30 +1,24 @@
 package org.example.fromaljolanda;
 
-import java.util.Arrays;
-
 public class CodingBat {
     public static void main(String[] args) {
-        int[] array = new int[] {5, 5, 10, 10, 4, 1, 2, 9, 7, 8};
 
+        /**
+         The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation.
+         We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+         sleepIn(false, false) → true
+         sleepIn(true, false) → false
+         sleepIn(false, true) → true
 
-    }
-}
-    /**
-    The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation.
-    We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
-    sleepIn(false, false) → true
-    sleepIn(true, false) → false
-    sleepIn(false, true) → true
-
-    public boolean sleepIn(boolean weekday, boolean vacation) {
-        if (!weekday || vacation) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-     */
+         public boolean sleepIn(boolean weekday, boolean vacation) {
+         if (!weekday || vacation) {
+         return true;
+         }
+         else {
+         return false;
+         }
+         }
+         */
 // ----------------------------------------------------------------------------
     /*
     We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling.
@@ -46,20 +40,20 @@ public class CodingBat {
     }
     */
 // ----------------------------------------------------------------------------
-    /**
-    Given two int values, return their sum. Unless the two values are the same, then return double their sum.
-    sumDouble(1, 2) → 3
-    sumDouble(3, 2) → 5
-    sumDouble(2, 2) → 8
+        /**
+         Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+         sumDouble(1, 2) → 3
+         sumDouble(3, 2) → 5
+         sumDouble(2, 2) → 8
 
-    public int sumDouble(int a, int b) {
-      int sum = a + b;
-      if (a == b) {
-        sum = sum * 2;
-      }
-      return sum;
-    }
-    */
+         public int sumDouble(int a, int b) {
+         int sum = a + b;
+         if (a == b) {
+         sum = sum * 2;
+         }
+         return sum;
+         }
+         */
 // ----------------------------------------------------------------------------
     /*
     Given an int n, return the absolute difference between n and 21,
@@ -78,22 +72,22 @@ public class CodingBat {
     }
     */
 // ----------------------------------------------------------------------------
-    /**
-    We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23.
-    We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
-    parrotTrouble(true, 6) → true
-    parrotTrouble(true, 7) → false
-    parrotTrouble(false, 6) → false
+        /**
+         We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23.
+         We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
+         parrotTrouble(true, 6) → true
+         parrotTrouble(true, 7) → false
+         parrotTrouble(false, 6) → false
 
-    public boolean parrotTrouble(boolean talking, int hour) {
-      if (talking && (hour < 7 || hour > 20)) {
-        return true;
-      }
-      else {
-        return false;
-      }
-    }
-    */
+         public boolean parrotTrouble(boolean talking, int hour) {
+         if (talking && (hour < 7 || hour > 20)) {
+         return true;
+         }
+         else {
+         return false;
+         }
+         }
+         */
 // ----------------------------------------------------------------------------
     /*
     Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
@@ -111,22 +105,22 @@ public class CodingBat {
     }
     */
 // ----------------------------------------------------------------------------
-    /**
-    Given an int n, return true if it is within 10 of 100 or 200.
-    Note: Math.abs(num) computes the absolute value of a number.
-    nearHundred(93) → true
-    nearHundred(90) → true
-    nearHundred(89) → false
+        /**
+         Given an int n, return true if it is within 10 of 100 or 200.
+         Note: Math.abs(num) computes the absolute value of a number.
+         nearHundred(93) → true
+         nearHundred(90) → true
+         nearHundred(89) → false
 
-    public boolean nearHundred(int n) {
-      if (Math.abs(100 - n) <= 10 || Math.abs(200 - n) <= 10) {
-        return true;
-      }
-      else {
-        return false;
-      }
-    }
-    */
+         public boolean nearHundred(int n) {
+         if (Math.abs(100 - n) <= 10 || Math.abs(200 - n) <= 10) {
+         return true;
+         }
+         else {
+         return false;
+         }
+         }
+         */
 // ----------------------------------------------------------------------------
     /*
     Given 2 int values, return true if one is negative and one is positive.
@@ -148,30 +142,18 @@ public class CodingBat {
     }
     */
 // ----------------------------------------------------------------------------
-    /**
-    Given a string, return a new string where "not " has been added to the front.
-    However, if the string already begins with "not", return the string unchanged.
-    Note: use .equals() to compare 2 strings.
-    notString("candy") → "not candy"
-    notString("x") → "not x"
-    notString("not bad") → "not bad"
+        /**
+         Given a string, return a new string where "not " has been added to the front.
+         However, if the string already begins with "not", return the string unchanged.
+         Note: use .equals() to compare 2 strings.
+         notString("candy") → "not candy"
+         notString("x") → "not x"
+         notString("not bad") → "not bad"
 
-     public String notString(String str) {
-         if (str.length() >= 3 && str.substring(0, 3).equals("not")) {
-             return str;
-         }
-         else {
-             return "not " + str;
-         }
-     }
-     */
-// ----------------------------------------------------------------------------
-    /*
-
-    */
+         */
 // ----------------------------------------------------------------------------
 // ----------------------------------------------------------------------------
 // ----------------------------------------------------------------------------
-
-
-
+// ----------------------------------------------------------------------------
+    }
+}
