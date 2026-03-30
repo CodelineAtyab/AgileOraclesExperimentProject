0c391adbc2be7ecfead05cd93b83d62d16f71aa3
abbhosni <binkhalidabdulahv@gmail.com>
2026-03-30 14:57:50 +0400

    // ─────────────────────────────────────────

--- src/main/java/org/example/fromabdullahhosni/EscapingMaze.java
+++ src/main/java/org/example/fromabdullahhosni/EscapingMaze.java
@@ -49,6 +49,33 @@ public class EscapingMaze {
             System.out.println();
         }
 
+        // ─────────────────────────────────────────
+        // STEP 2 — DECLARE START/END POSITION .
+        // ─────────────────────────────────────────
+        int startrow = 0;
+        int startColumn = 0;    //start
+
+        int exitRow = 0;
+        int exitcolum = 0;      //exit
+
+
+        for (int row = 0; row < array2d.length; row++) {
+            for (int col = 0; col < array2d[row].length; col++) {
+                if (array2d[row][col] == '@') {
+                    startrow = row;                 // found start row
+                    startColumn = col;                 // found start col
+                }
+                if (array2d[row][col] == 'E') {
+                    exitRow = row;                  // found exit row
+                    exitcolum = col;                  // found exit col
+                }
+            }
+        }
+
+        //handle error
+        if (startrow == -1 || exitRow == -1) {
+            System.out.println("Error: maze must have '@' and 'E'");
+        }
 
     }
 }
