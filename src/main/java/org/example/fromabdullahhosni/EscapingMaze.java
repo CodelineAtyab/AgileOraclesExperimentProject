b7c223b8170a2ef3d5c898edd2589dd4f05b85c1
abbhosni <binkhalidabdulahv@gmail.com>
2026-03-30 14:45:05 +0400

    start escape the maze @ task

---
+++ src/main/java/org/example/fromabdullahhosni/EscapingMaze.java
@@ -0,0 +1,11 @@
+package org.example.fromabdullahhosni;
+
+public class EscapingMaze {
+    public static void main(String[] args) {
+
+        // ─────────────────────────────────────────
+        // STEP 1 — LOAD MAZE.TXT AND PRINT 2D ARRAY
+        // ─────────────────────────────────────────
+
+    }
+}
