3aaa13b6c196a116980ccc7a7e01cab03c67b1c0
abbhosni <binkhalidabdulahv@gmail.com>
2026-03-31 12:02:00 +0400

    taking input from cmd

--- src/main/java/org/example/fromabdullahhosni/SnakeGame.java
+++ src/main/java/org/example/fromabdullahhosni/SnakeGame.java
@@ -3,7 +3,9 @@ package org.example.fromabdullahhosni;
 public class SnakeGame {
     public static void main(String[] args) {
 
-
+        //--------------------------------------------
+        // STEP 1 - TAKING INPUT .CMD COMMAND LINE .
+        //--------------------------------------------
         try {
             if (args.length > 0){
                 for (String input : args){
