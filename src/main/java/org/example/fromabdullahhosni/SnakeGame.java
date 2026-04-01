98ea073f57575d1cff99508b3601160754db1801
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-01 10:24:46 +0400

    print game board.

--- src/main/java/org/example/fromabdullahhosni/SnakeGame.java
+++ src/main/java/org/example/fromabdullahhosni/SnakeGame.java
@@ -3,6 +3,9 @@ package org.example.fromabdullahhosni;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class SnakeGame {
     public static void main(String[] args) {
@@ -55,5 +58,39 @@ public class SnakeGame {
             System.out.println("Cannot Initelize Game board");
             throw new RuntimeException(e);
         }
+
+        //print the board rows and columns
+        for (int row = 0 ;row<array2d.length ;row++){
+            for (int col = 0 ;col< array2d[0].length ;col++){
+                System.out.printf("%s", array2d[row][col]); //2d array is printing
+            }
+            System.out.println();
+        }
+
+//
+//
+//        // declaring snake position
+//        // position is having {x,y}
+//        int rowPosition = 7; //row is presenting x.
+//        int[] colPosition = {5,6,7,8,9}; //col is presenting y.
+//
+//
+//
+//        Queue<int[]> queue = new LinkedList<>();
+//
+////        for (int col: colPosition) {
+////            queue.add(new int[]{rowPosition, col});
+////            System.out.println(queue);
+////        }
+//
+//        for (int col: colPosition){
+//            queue.add(new int[]{rowPosition,col});
+//        }
+//        for (int[] position: queue){
+//            System.out.println(Arrays.toString(position));
+//        }
+//        System.out.println();
+////        array2d
+//        //moving right
     }
 }
