30d9f8cce36436b0abd18552fe448229b520a15f
Your Name <you@example.com>
2026-04-06 09:34:24 +0400

    all files fixed

--- src/main/java/org/example/fromalharith/Main.java
+++ src/main/java/org/example/fromalharith/Main.java
@@ -1,6 +1,42 @@
 package org.example.fromalharith;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Check 1 Check 2");
+        Path mazePath = null;
+        char[][] maze;
+
+        try {
+            mazePath = Path.of(Main.class.getResource("./data/.txt").toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            String fileContent = Files.readString(mazePath);
+            String[] lineOfFile = fileContent.split("\n");
+            int lineLength = lineOfFile[0].length();
+            maze = new char[lineOfFile.length][];
+
+            for (int row = 0; row < lineOfFile.length; row++) {
+                char[] currRow = lineOfFile[row].toCharArray();
+
+                for (int col = 0; col < currRow.length; col++) {
+                    maze[row][col] = currRow[col];
+                }
+            }
+            } catch(IOException e){
+                throw new RuntimeException(e);
+            }
+
+            for (int row=0; row<10; row++){
+                for (int col=0; col<10; col++)
+                    System.out.printf("%c",maze[row][col]);
+            }
+            System.out.println();
+        }
     }
-}
