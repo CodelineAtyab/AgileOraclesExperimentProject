e0b1aa5bf7a5ba43e963656dc779fd31b465bbaa
TibyanSaad <saad.tibyan02@gmail.com>
2026-04-08 15:29:20 +0400

    intial structure of the code

---
+++ src/main/java/org/example/fromtibyan/sprint5/internqueuesorteroop/BubbleSort.java
@@ -0,0 +1,22 @@
+package org.example.fromtibyan.sprint5.internqueuesorteroop;
+
+public class BubbleSort implements SortStrategy  {
+    @Override
+    public void sort(int[] priorities){
+        for (int i = 0; i < priorities.length; i++) {
+            for (int j = 0; j < priorities.length - 1; j++) {
+                if (priorities[j] > priorities[j + 1]) {
+                    // must compare variable with adjacent element
+                    int temp = priorities[j];// store min value
+                    priorities[j] = priorities[j + 1];// swap both values
+                    priorities[j + 1] = temp;// store in temporary variable for comparison
+                }
+            }
+        }
+        System.out.print("Intern queue: ");
+        // print first 3 min priorities
+        for (int i = 0; i < 3; i++) {
+            System.out.print(priorities[i] + " ");
+        }
+    }
+}
---
+++ src/main/java/org/example/fromtibyan/sprint5/internqueuesorteroop/InternQueueSorter.java
@@ -0,0 +1,43 @@
+package org.example.fromtibyan.sprint5.internqueuesorteroop;
+
+import java.util.Scanner;
+
+interface SortStrategy {
+    void sort(int[] priorities);
+}
+public class InternQueueSorter {
+    static int[] priorities;
+    public static void main(String[] args){
+        Scanner userInput = new Scanner(System.in);
+        if (args.length>0){
+            String[] comma = args[0].split(","); // remove comma separated values
+            priorities = new int[comma.length];
+            for (int i = 0; i < comma.length; i++) {
+                priorities[i] = Integer.parseInt(comma[i].trim()); // convert to array of integers
+            }
+            SortStrategy strategy = new SelectionSort();
+            strategy.sort(priorities);
+
+        }else {
+            System.out.println("Select a sorting algorithm.\n 1.Selection(default)\n 2.Bubble");
+            System.out.println("Choose a number");
+            int choice = userInput.nextInt();
+            System.out.println("Enter priority levels, each separated by a comma.(eg. 10,8,2,6,4)");
+            String PriorityList = userInput.next();
+            String[] comma = PriorityList.split(","); // remove comma separated values
+            priorities = new int[comma.length];
+            for (int i = 0; i < comma.length; i++) {
+                priorities[i] = Integer.parseInt(comma[i].trim()); // convert to array of integers
+            }
+            if (choice == 1){
+                SortStrategy strategy = new SelectionSort();
+                strategy.sort(priorities);
+            }else if(choice == 2){
+                SortStrategy strategy = new BubbleSort();
+                strategy.sort(priorities);
+            }else {
+                System.out.println("Select either 1 or 2.");
+            }
+        }
+    }
+}
