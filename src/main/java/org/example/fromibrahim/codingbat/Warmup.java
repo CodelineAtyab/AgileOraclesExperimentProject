5894d059026896172b620f796408f7cda32ca1d7
ibrahbi <ibra7bi@gmail.com>
2026-04-12 11:25:01 +0400

    Solving 7th Warmup Practice

--- src/main/java/org/example/fromibrahim/codingbat/Warmup.java
+++ src/main/java/org/example/fromibrahim/codingbat/Warmup.java
@@ -61,6 +61,14 @@ public class Warmup {
             return false;
         }
     }
+    // WarmUp 1 *******
+    public boolean nearHundred(int n) {
+        if ( n >= 90 && n <= 110 || n >= 190 && n <= 210 ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
 
