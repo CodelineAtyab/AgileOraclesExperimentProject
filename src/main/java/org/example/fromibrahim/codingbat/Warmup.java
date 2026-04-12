35d6f3f54a3f82faa3bd34891e0b04075101daf2
ibrahbi <ibra7bi@gmail.com>
2026-04-12 15:23:30 +0400

    Enhancements

--- src/main/java/org/example/fromibrahim/codingbat/Warmup.java
+++ src/main/java/org/example/fromibrahim/codingbat/Warmup.java
@@ -4,6 +4,26 @@ public class Warmup {
 
     public static void main(String[] args){
 
+        boolean canSleep = sleepIn(false, true);
+        System.out.println("Sleeping? " + canSleep);
+
+        boolean trouble = monkeyTrouble(true, true);
+        System.out.println("Are we in trouble? " + trouble);
+
+        int sum = sumDouble(2, 2);
+        System.out.println("Sum double = " + sum);
+
+        int diff = diff21(25);
+        System.out.println("Different = " + diff);
+
+        boolean parrot = parrotTrouble(true, 6);
+        System.out.println("Parrot trouble? " + parrot);
+
+        boolean isTen = makes10(5, 1);
+        System.out.println("Makes 10? " + isTen);
+
+        boolean near = nearHundred(93);
+        System.out.println("Near 100? " + near);
     }
 
     // WarmUp 1 *
@@ -16,7 +36,7 @@ public class Warmup {
     }
 
     // WarmUp 1 **
-    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+    public static boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
         if (aSmile && bSmile || ! aSmile && ! bSmile ){
             return true;
         }
@@ -26,7 +46,7 @@ public class Warmup {
     }
 
     // WarmUp 1 ***
-    public int sumDouble(int a, int b) {
+    public static int sumDouble(int a, int b) {
         int sum = a + b;
         if (a == b){
             return sum = sum * 2;
@@ -36,7 +56,7 @@ public class Warmup {
     }
 
     // WarmUp 1 ****
-    public int diff21(int n) {
+    public static int diff21(int n) {
         if (n <= 21){
             return (21 - n);
         } else {
@@ -45,7 +65,7 @@ public class Warmup {
     }
 
     // WarmUp 1 *****
-    public boolean parrotTrouble(boolean talking, int hour) {
+    public static boolean parrotTrouble(boolean talking, int hour) {
         if (talking &&  (hour < 7 || hour > 20)){
             return true;
         } else {
@@ -54,7 +74,7 @@ public class Warmup {
     }
 
     // WarmUp 1 ******
-    public boolean makes10(int a, int b) {
+    public static boolean makes10(int a, int b) {
         if (a == 10 || b == 10 || (a+b == 10)){
             return true;
         } else {
@@ -62,7 +82,7 @@ public class Warmup {
         }
     }
     // WarmUp 1 *******
-    public boolean nearHundred(int n) {
+    public static boolean nearHundred(int n) {
         if ( n >= 90 && n <= 110 || n >= 190 && n <= 210 ) {
             return true;
         } else {
