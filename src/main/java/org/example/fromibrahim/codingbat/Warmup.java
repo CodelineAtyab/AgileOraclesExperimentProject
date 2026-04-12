665291af597bf9e9d3495dc2a811fba14db78720
ibrahbi <ibra7bi@gmail.com>
2026-04-12 11:12:32 +0400

    Solving 6th Warmup Practice

--- src/main/java/org/example/fromibrahim/codingbat/Warmup.java
+++ src/main/java/org/example/fromibrahim/codingbat/Warmup.java
@@ -52,6 +52,16 @@ public class Warmup {
             return false;
         }
     }
+
+    // WarmUp 1 ******
+    public boolean makes10(int a, int b) {
+        if (a == 10 || b == 10 || (a+b == 10)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
 
 
