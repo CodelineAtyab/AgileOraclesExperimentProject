2ff305afc8e15c0b89ac9f1fde4b21107761e9a0
ibrahbi <ibra7bi@gmail.com>
2026-04-12 09:52:36 +0400

    Solving Fourth Warmup Practice

--- src/main/java/org/example/fromibrahim/codingbat/Warmup.java
+++ src/main/java/org/example/fromibrahim/codingbat/Warmup.java
@@ -34,5 +34,15 @@ public class Warmup {
             return sum;
         }
     }
+
+    // WarmUp 1 ****
+    public int diff21(int n) {
+        if (n <= 21){
+            return (21 - n);
+        } else {
+            return (n - 21)*2;
+        }
+    }
 }
+
 
