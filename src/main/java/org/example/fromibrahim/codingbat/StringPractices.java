d17988d1a92de85ccd6c67bff7aa42daadfe288b
ibrahbi <ibra7bi@gmail.com>
2026-04-12 14:23:29 +0400

    Enhancements

--- src/main/java/org/example/fromibrahim/codingbat/StringPractices.java
+++ src/main/java/org/example/fromibrahim/codingbat/StringPractices.java
@@ -2,14 +2,31 @@ package org.example.fromibrahim.codingbat;
 
 public class StringPractices {
     public static void main(String[] args){
+        // Practice 1
+        System.out.println(helloName("Ibrahim"));
+
+        //  Practice 2
+        System.out.println(makeAbba("Hi", "Bye"));
+
+        //  Practice 3
+        System.out.println(makeTags("i", "Yay"));
+
     }
     // String 1 - Practice 1
-    public String helloName(String Ibrahim) {
-        return "Hello " + Ibrahim + "!";
+    public static String helloName(String name) {
+        return "Hello " + name + "!";
     }
 
     // String 1 - Practice 2
-    public String makeAbba(String a, String b) {
+    public static String makeAbba(String a, String b) {
         return a + b + b + a;
     }
+
+    // String 1 - Practice 3
+    public static String makeTags(String tag, String word) {
+
+        return "<" + tag + ">" + word + "</" + tag + ">";
+
+    }
+
 }
