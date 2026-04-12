2e11510631ff539afbc5e3a317d3f89ba1b03bd9
KhadijaAli <alloughanikhadijaali@gmail.com>
2026-04-12 11:36:21 +0400

    Selection Sort

--- src/main/java/org/example/fromkhadija/sprint5/SortandServe.java
+++ src/main/java/org/example/fromkhadija/sprint5/SortandServe.java
@@ -15,6 +15,59 @@ public class SortandServe {
             ComplainPriorityNumber[i] = Integer.parseInt(args[i]);
             System.out.print(" " + ComplainPriorityNumber[i] + " ");
         }
+        boolean running = true;
+        while (running) {
+            System.out.println("\nPress:\n 1.Selection Sort");
+            int userInput = sc.nextInt();
+
+            if (userInput == 1) {
+                selectionSort(ComplainPriorityNumber);
+            }
+        }
+    }
+
+    //*****************************************(Selection Sort)*****************************************
+
+    public static void selectionSort(int[] ComplainPriorityNumber) {
+        System.out.println("*******************selectionSort*******************");
+
+        for (int currIter = 0; currIter < ComplainPriorityNumber.length - 1; currIter++) {
+
+            int minPriorityNum = currIter;
+
+            for (int i = currIter + 1; i < ComplainPriorityNumber.length; i++) {
+                if (ComplainPriorityNumber[i] < ComplainPriorityNumber[minPriorityNum]) {
+                    minPriorityNum = i;
+                }
+            }
+            // Swapping
+            int temp = ComplainPriorityNumber[currIter];
+            ComplainPriorityNumber[currIter] = ComplainPriorityNumber[minPriorityNum];
+            ComplainPriorityNumber[minPriorityNum] = temp;
+        }
+
+        System.out.println();
+        System.out.println();
+
+        System.out.print(" The Complain Priority Number After Sorting: ");
+
+        System.out.println();
+        System.out.println();
+        // After Sorting:
+        for (int i = 0; i < ComplainPriorityNumber.length; i++) {
+            System.out.print(" " + ComplainPriorityNumber[i] + " ");
+        }
+
+        System.out.println();
+        System.out.println();
+
+        //List The Lowest Three Numbers:
+        System.out.println("The lowest Three Numbers: ");
+        for (int i = 0; i < 3; i++) {
+
+            System.out.print(" " + ComplainPriorityNumber[i] + " ");
+        }
 
     }
 }
+
