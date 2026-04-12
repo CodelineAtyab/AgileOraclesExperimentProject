50e781914142de375e46753f4f42da9aa54315a6
ibrahbi <ibra7bi@gmail.com>
2026-04-12 09:42:55 +0400

    Solving First Warmup P:ractice

---
+++ src/main/java/org/example/fromibrahim/codingbat/Warmup.java
@@ -0,0 +1,18 @@
+package org.example.fromibrahim.codingbat;
+
+public class Warmup {
+
+    public static void main(String[] args){
+
+    }
+
+    // WarmUp 1 *
+    public static boolean sleepIn(boolean weekday, boolean vacation) {
+        if (!weekday || vacation) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
