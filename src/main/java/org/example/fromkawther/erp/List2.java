ad1470ad48d0eba77941ac0527ed989a447eac0a
kawther Almandhari <likawther17@gmail.com>
2026-04-12 14:18:16 +0400

    Update List2.java
    fix the merge conflict

--- src/main/java/org/example/fromkawther/erp/List2.java
+++ src/main/java/org/example/fromkawther/erp/List2.java
@@ -3,11 +3,6 @@ import java.util.Scanner;
 import java.util.ArrayList;
 public class List2{
 
-
-
-public class List2 {
-
-
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
