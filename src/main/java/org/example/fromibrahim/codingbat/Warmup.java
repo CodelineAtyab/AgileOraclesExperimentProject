3584add323cf75509519430d0c3004bcd2ecaff7
ibrahbi <ibra7bi@gmail.com>
2026-04-12 09:44:09 +0400

    Solving Second Warmup Practice

--- src/main/java/org/example/fromibrahim/codingbat/Warmup.java
+++ src/main/java/org/example/fromibrahim/codingbat/Warmup.java
@@ -14,5 +14,15 @@ public class Warmup {
             return false;
         }
     }
+
+    // WarmUp 1 **
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if (aSmile && bSmile || ! aSmile && ! bSmile ){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
 
