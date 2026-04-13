bdfe12c18461a9723dc6a7f1059c9874011e566f
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-04-13 15:23:25 +0400

    Java Console Tool – Intern-Queue Sorter completed

--- src/main/java/org/example/fromsaif/SPRINT5/SortAndServe.java
+++ src/main/java/org/example/fromsaif/SPRINT5/SortAndServe.java
@@ -1,5 +1,6 @@
 package org.example.fromsaif.SPRINT5;
 import java.util.Scanner;
+import java.util.Arrays;
 
 
 public class SortAndServe {
@@ -22,6 +23,8 @@ public class SortAndServe {
         // Read numbers as standard input
         if (rawInput.isEmpty()) {
             Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter complaint priority numbers separated by commas:");
+
             if (scanner.hasNextLine()) {
                 rawInput = scanner.nextLine();
             }
@@ -56,43 +59,51 @@ public class SortAndServe {
             System.out.println("Invalid: Use Selection or Bubble algorithm");
             return;
         }
+
+        System.out.println(Arrays.toString(numbers));
+        System.out.println("Intern queue: " + numbers[0] + ", " + numbers[1] + ", " + numbers[2]);
+
     }
 
-    public static void bubbleSort(int[] numbers) {}
+
+
+    // Bubble Sort algorithm
+    public static void bubbleSort(int[] numbers) {
+        int temporaryValue;
+
+        for (int i = numbers.length; i > 0; i--) {
+            for (int j = 0; j < i - 1; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    temporaryValue = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temporaryValue;
+                }
+            }
+        }
+    }
+
+
+    // Selection Sort algorithm
+
     public static void selectionSort(int[] numbers) {
         int smallestIndex;
         int temporaryValue;
 
         for (int i = 0; i < numbers.length - 1; i++) {
+            smallestIndex = i;
 
+            for (int j = i + 1; j < numbers.length; j++) // Finds the smallest value
+                if (numbers[j] < numbers[smallestIndex]) {
+                    smallestIndex = j;
+                }
+
+            if (smallestIndex != i) { // Swaps the smallest value in the correct position
+                temporaryValue = numbers[i];
+                numbers[i] = numbers[smallestIndex];
+                numbers[smallestIndex] = temporaryValue;
             }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //System.out.println("Program started.");
-        //System.out.println("Selected algorithm: " + algorithm);
-        //System.out.println("Raw input: " + rawInput);
+        }
     }
 }
 
