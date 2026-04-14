a7d3530947a57d4be15200ebe2a9bcf0660655e2
Sulaiman Al-Farsi <sulaimanalfarsi3060@gmail.com>
2026-04-14 16:42:13 +0400

    feat: implement Maze class with grid storage and helper methods

--- src/main/java/org/example/fromsulaiman/s5/escapethemaze/Maze.java
+++ src/main/java/org/example/fromsulaiman/s5/escapethemaze/Maze.java
@@ -1,4 +1,40 @@
 package org.example.fromsulaiman.s5.escapethemaze;
 
 public class Maze {
+    private char[][] grid;
+    private  int size;
+
+    public Maze(char[][]grid){
+        this.grid =grid;
+        this.size =grid.length;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public char getCell(int row ,int col){
+        return grid[row][col];
+    }
+
+    public void setCell(int row,int col,char value){
+        grid[row][col] = value;
+    }
+
+    public boolean isWall(int row,int col){
+        return grid[row][col] == '1';
+    }
+
+    public boolean isExit(int row, int col){
+        return grid[row][col] == 'E';
+    }
+
+    public boolean isValidMove(int row, int col){
+        return row >= 0 && col >= 0 && row < size && col < size && !isWall(row,col);
+    }
+
+    public char[][] getGrid(){
+        return grid;
+    }
+
 }
