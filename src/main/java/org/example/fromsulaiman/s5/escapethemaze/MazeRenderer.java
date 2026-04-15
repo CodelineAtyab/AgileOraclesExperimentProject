9a0a2ac03501b7aa6602ae40c24a0f01c0094272
Sulaiman Al-Farsi <sulaimanalfarsi3060@gmail.com>
2026-04-15 08:57:24 +0400

    feat: add MazeRenderer for console visualization

--- src/main/java/org/example/fromsulaiman/s5/escapethemaze/MazeRenderer.java
+++ src/main/java/org/example/fromsulaiman/s5/escapethemaze/MazeRenderer.java
@@ -1,4 +1,18 @@
 package org.example.fromsulaiman.s5.escapethemaze;
 
 public class MazeRenderer {
+    public static void render(Maze maze) {
+        char[][] grid = maze.getGrid();
+
+        for (char[] row : grid){
+            for (char cell: row){
+                System.out.print(cell+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void clear(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
 }
