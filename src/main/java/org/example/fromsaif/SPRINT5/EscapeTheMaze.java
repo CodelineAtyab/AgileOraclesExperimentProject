9e6d80c1e9e976f401072e217eaf78023e40d6f4
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-04-15 11:32:56 +0400

    Working on Maze validation

--- src/main/java/org/example/fromsaif/SPRINT5/EscapeTheMaze.java
+++ src/main/java/org/example/fromsaif/SPRINT5/EscapeTheMaze.java
@@ -12,7 +12,8 @@ public class EscapeTheMaze {
             System.out.println("Error: Please provide the correct file path.");
             return;
         }
-        // Handling command-line arguments
+
+        // ===================== Handling command-line arguments ======================
         String mazeFilePath = args[0];
         // Load the file
         try {
@@ -21,6 +22,7 @@ public class EscapeTheMaze {
             System.out.println("Maze file was read successfully.");
             System.out.println(lines);
 
+            // Converts file lines into 2D arrays
             char[][] maze = new char[lines.size()][];
             for (int i = 0; i < lines.size(); i++) {
                 maze[i] = lines.get(i).toCharArray(); // Convert one text row into one maze row
@@ -29,11 +31,42 @@ public class EscapeTheMaze {
             System.out.println("Maze converted to 2D array successfully.");
             System.out.println("First cell: " + maze[0][0]);
             System.out.println("Second row: " + new String(maze[1]));
+            validateMaze(maze);
 
-            } catch(IOException e){
-                System.out.println("Error: file could not be read.");
-            }
+        } catch (IOException e) {
+            System.out.println("Error: file could not be read.");
         }
     }
+
+    // =========================== MAZE VALIDATION ===============================
+
+    static void validateMaze(char[][] maze) {
+        // Validates if there's columns and rows
+        if (maze.length == 0) {
+            //throw new : stop the method immediately and report that the input is invalid
+            throw new IllegalArgumentException("Can't run: Maze must have at least one row.");
+        }
+        if (maze[0].length == 0) {
+            throw new IllegalArgumentException("Can't run: Maze must have at least one column.");
+        }
+        // Validate the no. of columns = no. of rows
+        int rows = maze.length;
+        int cols = maze[0].length;
+
+        if (rows != cols) {
+            throw new IllegalArgumentException("Maze must have the same number of rows and columns.");
+        }
+
+        // Validate the borders
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                boolean isBorder = row == 0 || row == rows - 1 || col == 0 || col == cols - 1;
+
+            }
+
+        }
+    }
+}
+
 
 
