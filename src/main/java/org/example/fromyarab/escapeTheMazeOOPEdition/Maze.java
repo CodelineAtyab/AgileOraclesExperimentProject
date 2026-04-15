69f701f5ab444c4dd11254794068bfdf9df36a7f
Ya'Rab Al-Mamari <115209826+JuniorPrice@users.noreply.github.com>
2026-04-15 09:30:32 +0400

    prepare needed files

---
+++ src/main/java/org/example/fromyarab/escapeTheMazeOOPEdition/Maze.java
@@ -0,0 +1,4 @@
+package org.example.fromyarab.escapeTheMazeOOPEdition;
+
+public class Maze {
+}
---
+++ src/main/java/org/example/fromyarab/escapeTheMazeOOPEdition/MazeApp.java
@@ -0,0 +1,7 @@
+package org.example.fromyarab.escapeTheMazeOOPEdition;
+
+public class MazeApp {
+    public static void main(String[] args){
+
+    }
+}
