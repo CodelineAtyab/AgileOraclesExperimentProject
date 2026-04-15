f93727db6ab2e3d7cd3c4b986ba0013994430983
Ya'Rab Al-Mamari <115209826+JuniorPrice@users.noreply.github.com>
2026-04-15 10:20:08 +0400

    prepare position class

--- src/main/java/org/example/fromyarab/escapeTheMazeOOPEdition/Position.java
+++ src/main/java/org/example/fromyarab/escapeTheMazeOOPEdition/Position.java
@@ -1,4 +1,26 @@
 package org.example.fromyarab.escapeTheMazeOOPEdition;
 
 public class Position {
+    private int row;
+    private int column;
+
+    // get row
+    public int getRow() {
+        return row;
+    }
+
+    // set row
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    // get column
+    public int getColumn() {
+        return column;
+    }
+
+    // set column
+    public void setColumn(int column) {
+        this.column = column;
+    }
 }
