9cd35541c02364191eed2b362422daec8b08cb58
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-04-18 20:51:25 +0400

    Main Class

---
+++ src/main/java/org/example/fromaljolanda/sprint5/escapeTheMazeOOPEdition/Main.java
@@ -0,0 +1,24 @@
+package org.example.fromaljolanda.sprint5.escapeTheMazeOOPEdition;
+
+public class Main {
+    public static void main(String[] args) {
+        // check argument
+        if (args.length != 1) {
+            System.out.println("Usage: java Main maze.txt");
+            return;
+        }
+
+        String path = args[0];
+
+        try {
+            // Load maze
+            Maze maze = MazeLoader.load(path);
+
+            // Solve maze
+            MazeSolver.solve(maze);
+        }
+        catch (Exception e) {
+            System.out.println("File not found!");
+        }
+    }
+}
