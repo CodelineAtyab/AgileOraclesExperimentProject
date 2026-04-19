0c364949f0fcd044ef424a7073f0fbcdaf7a0749
Ya'Rab Al-Mamari <yarop123456789@gmail.com>
2026-04-19 15:20:05 +0400

    maze loader class

--- src/main/java/org/example/fromyarab/escapeTheMazeOOPEdition/MazeLoader.java
+++ src/main/java/org/example/fromyarab/escapeTheMazeOOPEdition/MazeLoader.java
@@ -7,88 +7,68 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class MazeLoader {
-    ArrayList<ArrayList<String>> mazeGrid = new ArrayList<>();
-    private final String mazeFilePath;
 
-    public MazeLoader(String mazeFilePath){
-        this.mazeFilePath = mazeFilePath;
-    }
+    public Maze load(String filePath) throws FileNotFoundException {
+        ArrayList<ArrayList<String>> grid = new ArrayList<>();
+        File mazeFile = new File(filePath);
 
-    public ArrayList<ArrayList<String>> loadMaze(){
-        File mazeFile = new File(mazeFilePath);
-
-        try (Scanner scan = new Scanner(mazeFile)){
-            // read the maze file and store data in array list
+        try (Scanner scan = new Scanner(mazeFile)) {
             while (scan.hasNextLine()) {
                 ArrayList<String> line = new ArrayList<>(Arrays.asList(scan.nextLine().split("")));
-                mazeGrid.add(line);
+                grid.add(line);
             }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
         }
-        // validate maze and return it
-        boolean validMaze = validateMaze(mazeGrid);
-        if (validMaze){
-            return mazeGrid;
-        }
-        else {
-            return null;
-        }
+
+        return validate(grid);
     }
 
-    // function to validate the maze
-    public boolean validateMaze(ArrayList<ArrayList<String>> maze){
-        // check the validity of the maze data
+    private Maze validate(ArrayList<ArrayList<String>> grid) {
         int enPointCounter = 0;
         int exPointCounter = 0;
-        ArrayList<Integer> enPointCoordinate = new ArrayList<>();
-        ArrayList<Integer> exPointCoordinate = new ArrayList<>();
-        for (int i=0; i<maze.size(); i++){
-            for (int j=0; j<maze.get(i).size(); j++){
-                // check the characters
-                if (!maze.get(i).get(j).equals("1") && !maze.get(i).get(j).equals("0") && !maze.get(i).get(j).equals("@") && !maze.get(i).get(j).equals("E")){
-                    System.out.printf("Maze Not Valid!! -> Characters restriction violation...(Character Position: <i>%d <j>%d)\n",i,j);
-                    return false;
+        Position startPos = null;
+        Position exitPos = null;
+
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid.get(i).size(); j++) {
+                String cell = grid.get(i).get(j);
+
+                // validate characters
+                if (!cell.equals("1") && !cell.equals("0") && !cell.equals("@") && !cell.equals("E")) {
+                    throw new IllegalArgumentException(
+                            String.format("Maze Not Valid!! -> Characters restriction violation...(Character Position: <i>%d <j>%d)", i, j));
                 }
 
-                // check corners of the maze
-                if ((i==0 || i==maze.size()-1) && (j==0 || j==maze.get(i).size()-1) && (maze.get(i).get(j).equals("@") || maze.get(i).get(j).equals("E"))){
-                    System.out.printf("Maze Not Valid!! -> Border restriction violation...(Corner Position: <i>%d <j>%d)\n",i,j);
-                    return false;
+                // check corners
+                if ((i == 0 || i == grid.size() - 1) && (j == 0 || j == grid.get(i).size() - 1)
+                        && (cell.equals("@") || cell.equals("E"))) {
+                    throw new IllegalArgumentException(
+                            String.format("Maze Not Valid!! -> Border restriction violation...(Corner Position: <i>%d <j>%d)", i, j));
                 }
+
                 // check borders
-                if (i==0 || j==0 || i==maze.size()-1 || j==maze.get(i).size()-1){
-                    if (maze.get(i).get(j).equals("@")){
-                        enPointCounter += 1;
-                        enPointCoordinate.add(i);
-                        enPointCoordinate.add(j);
-                    }
-                    else if (maze.get(i).get(j).equals("E")){
-                        exPointCounter += 1;
-                        exPointCoordinate.add(i);
-                        exPointCoordinate.add(j);
-                    }
-                    else if (!maze.get(i).get(j).equals("1")){
-                        System.out.printf("Maze Not Valid!! -> Border restriction violation...(Border Position: <i>%d <j>%d)\n",i,j);
-                        return false;
+                if (i == 0 || j == 0 || i == grid.size() - 1 || j == grid.get(i).size() - 1) {
+                    if (cell.equals("@")) {
+                        enPointCounter++;
+                        startPos = new Position(i, j);
+                    } else if (cell.equals("E")) {
+                        exPointCounter++;
+                        exitPos = new Position(i, j);
+                    } else if (!cell.equals("1")) {
+                        throw new IllegalArgumentException(
+                                String.format("Maze Not Valid!! -> Border restriction violation...(Border Position: <i>%d <j>%d)", i, j));
                     }
                 }
             }
         }
-        // check number of entry and exit points
-        if (enPointCounter != 1 || exPointCounter != 1){
-            System.out.println("Maze Not Valid!! -> # Entry/Exit point restriction violation...");
-            return false;
+
+        if (enPointCounter != 1 || exPointCounter != 1) {
+            throw new IllegalArgumentException("Maze Not Valid!! -> # Entry/Exit point restriction violation...");
         }
 
-        // check coordinates of entry and exit points
-        if (enPointCoordinate.get(0).equals(exPointCoordinate.get(0)) || enPointCoordinate.get(1).equals(exPointCoordinate.get(1))) {
-            System.out.println("Maze Not Valid!! -> Entry/Exit point coordinate restriction violation...");
-            return false;
+        if (startPos.getRow() == exitPos.getRow() || startPos.getCol() == exitPos.getCol()) {
+            throw new IllegalArgumentException("Maze Not Valid!! -> Entry/Exit point coordinate restriction violation...");
         }
 
-        // the maze passed all tests
-        return true;
+        return new Maze(grid, startPos, exitPos);
     }
-
 }
