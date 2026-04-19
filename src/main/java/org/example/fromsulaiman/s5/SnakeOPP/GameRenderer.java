437413a5ff371b23097d17163b065903a8ad88b0
Sulaiman Al-Farsi <sulaimanalfarsi3060@gmail.com>
2026-04-19 10:38:43 +0400

    feat: add GameRenderer to print map to console

--- src/main/java/org/example/fromsulaiman/s5/SnakeOPP/GameRenderer.java
+++ src/main/java/org/example/fromsulaiman/s5/SnakeOPP/GameRenderer.java
@@ -1,4 +1,17 @@
 package org.example.fromsulaiman.s5.SnakeOPP;
 
 public class GameRenderer {
+    public void render(GameMap gameMap) {
+        char[][] grid = gameMap.getGrid();
+        for (char[] row : grid) {
+            StringBuilder builder = new StringBuilder();
+            for (int col = 0; col < row.length; col++) {
+                builder.append(row[col]);
+                if (col < row.length - 1) {
+                    builder.append(' ');
+                }
+            }
+            System.out.println(builder);
+        }
+    }
 }
