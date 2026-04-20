3bb2249c74b843ae5e79e4153e67b1e0648c20a3
TibyanSaad <saad.tibyan02@gmail.com>
2026-04-20 20:55:12 +0400

    addede ticket class

---
+++ src/main/java/org/example/fromtibyan/sprint6/CLIERPOOP/Ticket.java
@@ -0,0 +1,10 @@
+package org.example.fromtibyan.sprint6.CLIERPOOP;
+
+abstract public class Ticket {
+    private String id;
+    private String comment;
+
+    Ticket(String id, String comment){
+
+    }
+}
