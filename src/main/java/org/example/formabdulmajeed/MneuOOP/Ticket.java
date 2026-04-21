1cea23325f3a95e792fe7e45b3d2490585a67e8a
abdulmajeedalbalushi <almajeed9266@gmail.com>
2026-04-22 00:01:04 +0400

    to branch

---
+++ src/main/java/org/example/formabdulmajeed/MneuOOP/Ticket.java
@@ -0,0 +1,15 @@
+package org.example.formabdulmajeed.MneuOOP;
+
+public abstract class Ticket {
+    protected String id;
+    protected String comment;
+
+    public abstract String setId();
+    //public abstract String getId();
+    //public abstract String setComment();
+
+    public abstract String setComment(String comments);
+
+    //public abstract String setType(String soundIssue);
+}
+
