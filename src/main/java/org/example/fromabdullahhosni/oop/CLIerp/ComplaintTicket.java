1209b2bbf00acb99f3d2b047692ac7192eb4fd75
aaabAlhosni <binkhalidabdullahv@gmail.com>
2026-04-22 12:16:30 +0400

    Updated class FeedbackTicket, Ovverride method getid()

--- src/main/java/org/example/fromabdullahhosni/oop/CLIerp/ComplaintTicket.java
+++ src/main/java/org/example/fromabdullahhosni/oop/CLIerp/ComplaintTicket.java
@@ -1,24 +1,30 @@
 package org.example.fromabdullahhosni.oop.CLIerp;
 
 public class ComplaintTicket extends Ticket {
-    // Declare Private String
     private String type;
     private static int numberOfTickets = 0;
 
-    //constructor
-    public ComplaintTicket(String id, String comment, String type) {
-        super(id, comment);     // super method reffers gets id, comment from parent class.
+    public ComplaintTicket(String comment, String type) {
+        super(comment);
         this.type = type;
-        numberOfTickets++;      //counter is Adding value.
-
+        numberOfTickets++;
     }
 
-    // method
     public static int getNumberOfTickets() {
         return numberOfTickets;
     }
-    // method
+
     public String getType() {
         return type;
+    }
+
+    // override - polymorphism
+    @Override
+    public String getId() {
+        return buildPrefixedId();
+    }
+
+    private String buildPrefixedId() {
+        return "C-" + java.util.UUID.randomUUID().toString();
     }
 }
