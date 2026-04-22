a31a6fd60b851fcc94d84393965a878d68078034
safaalmaamari0-source <safaalmaamari0@gmail.com>
2026-04-22 12:00:49 +0400

    create abstract parent Ticket class

---
+++ src/main/java/org/example/fromsafaalmaamari/sprint6/Ticket.java
@@ -0,0 +1,29 @@
+package org.example.fromsafaalmaamari.sprint6;
+
+public class Ticket {
+        private String id;
+        private String comment;
+
+        //constructor
+        public Ticket(String id, String comment){
+            this.id = id;
+            this.comment = comment;
+        }
+        //get id
+        public String getId(){
+            return id;
+        }
+        //Set id
+        public void setId(String id){
+            this.id = id;
+        }
+        //Get comment
+        public String getComment(){
+            return comment;
+        }
+        //Set comment
+        public void setComment(String comment){
+            this.comment = comment;
+        }
+    }
+
