674374f16cbdad758f2dfe6a74b410d53c4e5c0d
Sulaiman Al-Farsi <sulaimanalfarsi3060@gmail.com>
2026-04-26 14:41:09 +0400

    init: FeedbackTicket skeleton

--- src/main/java/org/example/fromsulaiman/s6/polymorphicTicketIDs/FeedbackTicket.java
+++ src/main/java/org/example/fromsulaiman/s6/polymorphicTicketIDs/FeedbackTicket.java
@@ -1,4 +1,16 @@
 package org.example.fromsulaiman.s6.polymorphicTicketIDs;
 
-public class FeedbackTicket {
+import java.util.UUID;
+
+public class FeedbackTicket extends Ticket {
+
+    public FeedbackTicket(String comment) {
+        super(comment);
+    }
+
+    @Override
+    public String getId() {
+        return null;
+    }
+
 }
