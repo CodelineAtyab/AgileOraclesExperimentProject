423f4a5ec637f2df0ef729158f7a4b4511a5a0c5
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-04-26 22:22:59 +0400

    Add constructor with id and comment to Ticket

--- src/main/java/org/example/fromaljolanda/sptint6/ticketing/Ticket.java
+++ src/main/java/org/example/fromaljolanda/sptint6/ticketing/Ticket.java
@@ -3,4 +3,9 @@ package org.example.fromaljolanda.sptint6.ticketing;
 public abstract class Ticket {
     private String id;
     private String comment;
+
+    public Ticket(String id, String comment) {
+        this.id = id;
+        this.comment = comment;
+    }
 }
