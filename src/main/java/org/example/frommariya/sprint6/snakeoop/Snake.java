4105779e4af28013da988b464c66dfc43110e156
Mariya <lamkia2001@gmail.com>
2026-04-26 17:30:08 +0400

    Created the necessery classes for the Snake java file

---
+++ src/main/java/org/example/frommariya/sprint6/snakeoop/Snake.java
@@ -0,0 +1,49 @@
+package org.example.frommariya.sprint6.snakeoop;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Snake {
+    public LinkedList<Position> snakeBody;
+    public Direction currDirection;
+
+    public Snake(){
+
+    }
+
+    public void initializing(){
+
+    }
+
+    public void initializeCenter(){
+
+    }
+
+    public Position getSnakeHead(){
+
+    }
+
+    public Position getSnakeTail(){
+
+    }
+
+    public List<Position> getSnakeBody(){
+
+    }
+
+    public boolean movement(){
+
+    }
+
+    public List<Direction> getValidMovement(){
+
+    }
+
+    public int getSnakeLength(){
+
+    }
+
+    public Direction getcurrDirection(){
+
+    }
+}
