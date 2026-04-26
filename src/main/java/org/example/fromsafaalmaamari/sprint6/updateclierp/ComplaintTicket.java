d58106a6c2e58025b51a6579ec8906adce2e7206
safaalmaamari0-source <safaalmaamari0@gmail.com>
2026-04-26 23:21:06 +0400

    create complaint with prefix c-

---
+++ src/main/java/org/example/fromsafaalmaamari/sprint6/updateclierp/ComplaintTicket.java
@@ -0,0 +1,40 @@
+package org.example.fromsafaalmaamari.sprint6.updateclierp;
+
+
+import java.util.UUID;
+
+public class ComplaintTicket extends Ticket {
+    private String type;
+    private static int numberOfTickets = 0;
+
+    public ComplaintTicket (String id, String comment, String type){
+        super(id, comment);
+        this.type = type;
+        numberOfTickets++;
+    }
+    //get type
+    public String getType(){
+        return type;
+    }
+    //Set type
+    public void setType(String type){
+        this.type = type;
+    }
+    //Method to get number of Tickets
+    public static int getNumberOfTickets(){
+        return numberOfTickets;
+    }
+
+    private String buildPrefixedId(){
+        return "C-";
+    }
+
+    @Override
+    public String getId() {
+        return buildPrefixedId() + UUID.randomUUID().toString();
+    }
+
+    public String toString(){
+        return "[Complaint] id=" + getId() + ", comment=\"" + getComment() + "\"" + ", type=\"" + type + "\"";
+    }
+}
