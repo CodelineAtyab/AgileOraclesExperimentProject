426f9ab822e53a9a117685d64922d4639432835c
Sulaiman Al-Farsi <sulaimanalfarsi3060@gmail.com>
2026-04-26 14:45:17 +0400

    init: TicketTest

--- src/main/java/org/example/fromsulaiman/s6/polymorphicTicketIDs/TicketTest.java
+++ src/main/java/org/example/fromsulaiman/s6/polymorphicTicketIDs/TicketTest.java
@@ -1,4 +1,12 @@
 package org.example.fromsulaiman.s6.polymorphicTicketIDs;
 
 public class TicketTest {
+    public static void main(String[] args) {
+
+        Ticket t1 = new ComplaintTicket("Login page is broken on mobile.");
+        Ticket t2 = new FeedbackTicket("Please add a dark mode.");
+
+        System.out.println(t1.getId());
+        System.out.println(t2.getId());
+    }
 }
