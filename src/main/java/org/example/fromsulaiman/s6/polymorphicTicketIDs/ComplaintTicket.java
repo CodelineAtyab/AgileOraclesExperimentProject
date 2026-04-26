ca06f34ca354d06f1a555ab1afbcf2535ab35cc8
Sulaiman Al-Farsi <sulaimanalfarsi3060@gmail.com>
2026-04-26 14:32:15 +0400

    init: ComplaintTicket skeleton

--- src/main/java/org/example/fromsulaiman/s6/polymorphicTicketIDs/ComplaintTicket.java
+++ src/main/java/org/example/fromsulaiman/s6/polymorphicTicketIDs/ComplaintTicket.java
@@ -1,4 +1,12 @@
 package org.example.fromsulaiman.s6.polymorphicTicketIDs;
 
-public class ComplaintTicket {
+public class ComplaintTicket extends Ticket {
+    public ComplaintTicket(String comment) {
+        super(comment);
+    }
+
+    @Override
+    public String getId() {
+        return null;
+    }
 }
