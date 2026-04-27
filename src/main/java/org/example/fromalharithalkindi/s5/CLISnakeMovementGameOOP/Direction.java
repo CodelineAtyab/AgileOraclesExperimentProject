2735fa91107c5c9ee77fcfec581cdeef33a7f7e1
AL-Harith AL-Kindi <kindaking99@gmail.com>
2026-04-27 11:31:40 +0400

    Direction sloved

--- src/main/java/org/example/fromalharithalkindi/s5/CLISnakeMovementGameOOP/Direction.java
+++ src/main/java/org/example/fromalharithalkindi/s5/CLISnakeMovementGameOOP/Direction.java
@@ -1,4 +1,8 @@
 package org.example.fromalharithalkindi.s5.CLISnakeMovementGameOOP;
 
 public class Direction {
+    public static final String UP = "up";
+    public static final String DOWN = "down";
+    public static final String LEFT = "left";
+    public static final String RIGHT = "right";
 }
