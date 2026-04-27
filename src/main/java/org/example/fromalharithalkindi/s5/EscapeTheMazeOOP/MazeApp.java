f4bed11193ca4a4b460a9439a2d6b3fc81f5548b
AL-Harith AL-Kindi <kindaking99@gmail.com>
2026-04-27 09:42:37 +0400

    calling all function in main = maze app

--- src/main/java/org/example/fromalharithalkindi/s5/EscapeTheMazeOOP/MazeApp.java
+++ src/main/java/org/example/fromalharithalkindi/s5/EscapeTheMazeOOP/MazeApp.java
@@ -1,10 +1,19 @@
 package org.example.fromalharithalkindi.s5.EscapeTheMazeOOP;
 
 public class MazeApp {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        try {
+            if (args.length == 0) {
+                System.out.println("Usage: java MazeApp <maze-file>");
+                return;
+            }
 
+            Maze maze = MazeLoader.load(args[0]);
+            MazeSolver solver = new MazeSolver(maze);
+            solver.solve();
 
-
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
