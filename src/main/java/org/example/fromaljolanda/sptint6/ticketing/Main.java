5f669474df9bdea9583d8581053ec3f5ba01a44a
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-04-27 08:46:36 +0400

    Print complaint ticket count before creation

--- src/main/java/org/example/fromaljolanda/sptint6/ticketing/Main.java
+++ src/main/java/org/example/fromaljolanda/sptint6/ticketing/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Ticket> tickets = new ArrayList<>();
 
+        System.out.println("Complaint tickets so far: " + ComplaintTicket.getNumberOfTickets());
     }
 }
