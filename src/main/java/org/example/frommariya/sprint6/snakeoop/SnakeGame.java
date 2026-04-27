efde4c47cea627550f5efaab53c9fb34cc5ec9a6
Mariya <lamkia2001@gmail.com>
2026-04-27 11:57:05 +0400

    Created the necessery classes for the SnakeGame java file

---
+++ src/main/java/org/example/frommariya/sprint6/snakeoop/SnakeGame.java
@@ -0,0 +1,43 @@
+package org.example.frommariya.sprint6.snakeoop;
+
+import java.util.List;
+
+public class SnakeGame {
+    public static String Map_File="src/main/java/org/example/frommariya/sprint6/snakeoop/map.txt";
+
+    public GameMap gameMap;
+    public Snake snake;
+
+    public SnakeGame(){
+
+    }
+
+    public void loadCreateGame() throws Exception{
+
+    }
+
+    public void InitGame(){
+
+    }
+
+    public boolean moveExecution(Direction direction, int snakeSteps){
+
+    }
+
+    public void availableDirections(){
+
+    }
+
+    public void gameSaving(){
+
+    }
+
+
+    public void showGame(){
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}
