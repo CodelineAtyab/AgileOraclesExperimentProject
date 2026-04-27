5deb535087446e018a24debe531af7dd98e2a966
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-04-27 15:08:02 +0400

    Added drawMaze function

--- src/main/java/org/example/fromsaif/SPRINT5/EscapeTheMazeOOP/MazeRenderer.java
+++ src/main/java/org/example/fromsaif/SPRINT5/EscapeTheMazeOOP/MazeRenderer.java
@@ -1,8 +1,14 @@
 package org.example.fromsaif.SPRINT5.EscapeTheMazeOOP;
 
 public class MazeRenderer {
-
     public void drawMaze(Maze maze) {
+        for (int row = 0; row < maze.getRows(); row++) {
+            for (int col = 0; col < maze.getCols(); col++) {
+                Position position = new Position(row, col);
+                System.out.print(maze.getCell(position));
+            }
+            System.out.println();
+        }
 
     }
 }
