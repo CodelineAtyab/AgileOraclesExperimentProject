0047458163024fe2d27128b4b8581073ce14646c
Mariya <lamkia2001@gmail.com>
2026-04-27 16:48:24 +0400

    Added FeedbackTicket class with rating validation

--- src/main/java/org/example/frommariya/sprint6/clierpenforceencapsulation/FeedbackTicket.java
+++ src/main/java/org/example/frommariya/sprint6/clierpenforceencapsulation/FeedbackTicket.java
@@ -1,4 +1,32 @@
 package org.example.frommariya.sprint6.clierpenforceencapsulation;
 
-public class FeedbackTicket {
+public class FeedbackTicket extends Ticket {
+    private int rating;
+
+    public FeedbackTicket(String comment, String id, int rating) {
+        super(comment, id);
+
+        //validate the ration (1-5)
+        if (rating < 1 || rating > 5) {
+            throw new IllegalArgumentException("Rating between 1 and 5");
+        }
+        this.rating = rating;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        if (rating < 1 || rating > 5) {
+            throw new IllegalArgumentException("Rating between 1 and 5");
+        }
+        this.rating = rating;
+
+    }
+    @Override
+    public String toString(){
+        return
+                String.format("[Feedback ] id"+getId()+", comment="+getComment()+", rating="+getRating());
+    }
 }
