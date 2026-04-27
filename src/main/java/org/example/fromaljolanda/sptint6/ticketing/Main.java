78e3da71bc0d3e92ea4def8dd2b0ac3fb8a4c2a7
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-04-27 08:45:01 +0400

    Initialize ArrayList to store Ticket objects

---
+++ src/main/java/org/example/fromaljolanda/sptint6/ticketing/Main.java
@@ -0,0 +1,10 @@
+package org.example.fromaljolanda.sptint6.ticketing;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Ticket> tickets = new ArrayList<>();
+
+    }
+}
