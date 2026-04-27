fd9595d6c9ea5d5549621fb111f07e3095658afd
Mariya <lamkia2001@gmail.com>
2026-04-27 11:42:45 +0400

    Added FilePersistance: Handles saving and loading the map to/from map.txt to maintain game state

---
+++ src/main/java/org/example/frommariya/sprint6/snakeoop/FilePersistence.java
@@ -0,0 +1,38 @@
+package org.example.frommariya.sprint6.snakeoop;
+
+import java.io.*;
+import java.nio.file.*;
+
+public class FilePersistence {
+    public static void mapSave(GameMap gameMap,String mapFilePath) throws IOException{
+        StringBuilder con=new StringBuilder();
+        char[][] mapGrid = gameMap.getGrid();
+
+
+        for(int x=0;x<gameMap.getRows();x++){
+            for(int y=0;y<gameMap.getColumns();y++){
+                con.append(mapGrid[x][y]);
+                if(y<gameMap.getColumns()-1){
+                    con.append(" ");
+                }
+            }
+            if(x<gameMap.getRows()-1){
+                con.append("\n");
+            }
+        }
+
+        Path snakePath=Paths.get(mapFilePath);
+
+        if(snakePath.getParent()!=null){
+            Files.createDirectories(snakePath.getParent());
+        }
+        Files.writeString(snakePath,con.toString());
+
+    }
+
+    public static GameMap MapLoader(String mapFilepath) throws IOException{
+        return
+                MapLoader.MapLoader(mapFilepath);
+    }
+
+}
