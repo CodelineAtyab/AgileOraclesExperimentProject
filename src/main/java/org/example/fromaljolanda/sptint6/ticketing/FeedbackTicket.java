04b6f496acbe6c9b4a4382abc61effed5943a056
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-04-27 08:27:01 +0400

    Override toString method

--- src/main/java/org/example/fromaljolanda/sptint6/ticketing/FeedbackTicket.java
+++ src/main/java/org/example/fromaljolanda/sptint6/ticketing/FeedbackTicket.java
@@ -15,4 +15,11 @@ public class FeedbackTicket extends Ticket {
     public int getRating() {
         return rating;
     }
+
+    @Override
+    public String toString() {
+        return "[Feedback] id = " + getId()
+                + ", comment = \"" + getComment() + "\""
+                + ", rating = \"" + getRating() + "\"" ;
+    }
 }
