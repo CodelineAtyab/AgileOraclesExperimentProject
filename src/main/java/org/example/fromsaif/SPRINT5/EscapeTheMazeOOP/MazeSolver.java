c22f757e3bf5362ca83fa0f0f856eb711892290f
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-04-27 15:27:32 +0400

    Added MazeSolver imports and setups

--- src/main/java/org/example/fromsaif/SPRINT5/EscapeTheMazeOOP/MazeSolver.java
+++ src/main/java/org/example/fromsaif/SPRINT5/EscapeTheMazeOOP/MazeSolver.java
@@ -1,5 +1,7 @@
 package org.example.fromsaif.SPRINT5.EscapeTheMazeOOP;
 
+import org.example.fromsaif.SPRINT5.EscapeTheMaze;
+
 import java.util.Stack;
 
 public class MazeSolver {
@@ -13,5 +15,22 @@ public class MazeSolver {
         this.renderer = renderer;
         this.visited = new boolean[maze.getRows()][maze.getCols()];
         this.stack = new Stack<>();
+    }
+
+    public boolean solve() {
+        return false;
+    }
+
+    private Position findStart(){
+        for (int row = 0; row < maze.getRows(); row++) {
+            for (int col = 0; col < maze.getCols(); col++) {
+                Position position = new Position (row, col);
+
+                if (maze.getCell(position) == '@') {
+                    return position;
+                }
+            }
+        }
+        throw new IllegalArgumentException("Maze must contain '@' as the starting point.");
     }
 }
