2637f601ae5f2cfdcc6cf309fb746621916e6eed
ibrahbi <ibra7bi@gmail.com>
2026-04-28 16:50:47 +0400

    Creating rateing & constructor field in FeedbackTicket class

--- src/main/java/org/example/fromibrahim/OOP/refactor/cli/erp/FeedbackTicket.java
+++ src/main/java/org/example/fromibrahim/OOP/refactor/cli/erp/FeedbackTicket.java
@@ -1,4 +1,14 @@
 package org.example.fromibrahim.OOP.refactor.cli.erp;
 
-public class FeedbackTicket {
+public class FeedbackTicket extends Ticket {
+
+
+    private int rating;
+
+    //  Constructor
+    public FeedbackTicket(String id, String comment, int rating) {
+
+        // Always call super() first to initialise inherited fields
+        super(id, comment);
+    }
 }
