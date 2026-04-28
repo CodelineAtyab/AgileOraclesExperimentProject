c9b0f899c27da9ad3c22d6e1ca36df61e253b60b
AL-Harith AL-Kindi <kindaking99@gmail.com>
2026-04-28 15:40:20 +0400

    ticket class done

--- src/main/java/org/example/fromalharithalkindi/s5/refactorExistingCLIERP/Ticket.java
+++ src/main/java/org/example/fromalharithalkindi/s5/refactorExistingCLIERP/Ticket.java
@@ -1,5 +1,26 @@
 package org.example.fromalharithalkindi.s5.refactorExistingCLIERP;
 
 abstract class Ticket {
+    private  static int numOfTickets=0;
+
+    private final String ticketId;
+    private final String comment;
+
+    public Ticket(String ticketId,String comment)
+    {
+        this.ticketId=ticketId;
+        this.comment=comment;
+        numOfTickets++;
+    }
+    public String getTicketId() {
+        return ticketId;
+
+    }
+    public String getComment(){
+        return comment;
+    }
+    public static int getNumOfTickets(){
+        return numOfTickets;
+    }
 
 }
