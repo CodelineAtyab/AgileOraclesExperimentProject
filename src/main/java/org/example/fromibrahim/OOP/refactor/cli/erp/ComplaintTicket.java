211592594c7bc121e79fb5a44d6d26e6e8267aa1
ibrahbi <ibra7bi@gmail.com>
2026-04-28 16:32:22 +0400

    Creating type & constructor field in ComplaintTicket class

--- src/main/java/org/example/fromibrahim/OOP/refactor/cli/erp/ComplaintTicket.java
+++ src/main/java/org/example/fromibrahim/OOP/refactor/cli/erp/ComplaintTicket.java
@@ -1,4 +1,20 @@
 package org.example.fromibrahim.OOP.refactor.cli.erp;
 
-public class ComplaintTicket {
+public class ComplaintTicket extends Ticket {
+
+    private String type;
+
+    private static int numberOfTickets = 0;
+
+    public ComplaintTicket(String id, String comment, String type) {
+        // super() MUST be the very first statement.
+        // It calls Ticket(id, comment) to initialise the inherited fields.
+        super(id, comment);
+
+        this.type = type;
+
+        // Increment the shared counter every time a new object is born.
+        numberOfTickets++;
+    }
+
 }
