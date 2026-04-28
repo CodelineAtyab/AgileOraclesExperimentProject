dfbefe5a4f1619862eab293597e6c8eb77f79c75
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-04-28 09:51:58 +0400

    Added MazeRenderer current position

--- src/main/java/org/example/fromsaif/SPRINT5/EscapeTheMazeOOP/MazeRenderer.java
+++ src/main/java/org/example/fromsaif/SPRINT5/EscapeTheMazeOOP/MazeRenderer.java
@@ -17,4 +17,21 @@ public class MazeRenderer {
             System.out.println();
         }
     }
+
+    public void drawMaze(Maze maze, Position current) {
+        for (int row = 0; row < maze.getRows(); row++) {
+            for (int col = 0; col < maze.getCols(); col++) {
+                Position position = new Position(row, col);
+
+                if (row == current.getRow() && col == current.getCol()) {
+                    System.out.print('@');
+                } else if (maze.getCell(position) == '@') {
+                    System.out.print('0');
+                } else {
+                    System.out.print(maze.getCell(position));
+                }
+            }
+            System.out.println();
+        }
+    }
 }
