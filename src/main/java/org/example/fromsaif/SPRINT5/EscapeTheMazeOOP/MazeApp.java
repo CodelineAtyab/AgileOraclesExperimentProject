afbb7760b84a2259ee33b8d48dd66246e3c5cba7
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-04-28 10:47:07 +0400

    Finalized MazeApp main flow

--- src/main/java/org/example/fromsaif/SPRINT5/EscapeTheMazeOOP/MazeApp.java
+++ src/main/java/org/example/fromsaif/SPRINT5/EscapeTheMazeOOP/MazeApp.java
@@ -9,5 +9,24 @@ public class MazeApp {
             return;
         }
         String mazeFilePath = args[0];
+
+        try {
+            MazeLoader loader = new MazeLoader();
+            Maze maze = loader.loadMaze(mazeFilePath);
+            MazeRenderer renderer = new MazeRenderer();
+            MazeSolver solver = new MazeSolver(maze, renderer);
+
+            boolean solved = solver.solve();
+
+            if (solved) {
+                System.out.println("Maze Solved!");
+            } else {
+                System.out.println("No path found.");
+            }
+        } catch (IOException e) {
+            System.out.println("Error: Could not read file");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
