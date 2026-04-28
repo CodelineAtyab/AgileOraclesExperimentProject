bc92bdc41fecfe2a7b87b0814488adfe9e9bdb2d
AL-Harith AL-Kindi <kindaking99@gmail.com>
2026-04-28 16:00:24 +0400

    ComplaintTicke done

--- src/main/java/org/example/fromalharithalkindi/s5/refactorExistingCLIERP/ComplaintTicket.java
+++ src/main/java/org/example/fromalharithalkindi/s5/refactorExistingCLIERP/ComplaintTicket.java
@@ -1,4 +1,23 @@
 package org.example.fromalharithalkindi.s5.refactorExistingCLIERP;
 
-public class ComplaintTicket {
+public class ComplaintTicket extends Ticket {
+
+    private final String type;
+
+    public ComplaintTicket(String ticketID, String comment, String type) {
+        super(ticketID, comment);
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return "[complaint] id = " + getTicketId() + "\n"
+                + ", comment=\"" + getComment() + "\" \n"
+                + ", type=\"" + type + "\"";
+    }
+
 }
