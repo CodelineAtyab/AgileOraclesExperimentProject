8a857f6cd659f1b3ab49a8d83e35c12f5c91ab23
ibrahbi <ibra7bi@gmail.com>
2026-04-28 17:00:04 +0400

    Creating counter in Main class

--- src/main/java/org/example/fromibrahim/OOP/refactor/cli/erp/Main.java
+++ src/main/java/org/example/fromibrahim/OOP/refactor/cli/erp/Main.java
@@ -1,4 +1,16 @@
 package org.example.fromibrahim.OOP.refactor.cli.erp;
 
+import java.util.ArrayList;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        //  Step 1 : Show counter BEFORE any objects are created
+        System.out.println("Complaint tickets so far: " + ComplaintTicket.getNumberOfTickets());   // static call
+
+        System.out.println();
+        System.out.println("--- Creating tickets ---");
+
+    }
 }
