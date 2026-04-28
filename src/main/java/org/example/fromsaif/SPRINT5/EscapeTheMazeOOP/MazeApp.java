f0ffc0fb4e25f537d36073386b734caf5ced649d
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-04-28 10:31:07 +0400

    Added MazeApp main flow

--- src/main/java/org/example/fromsaif/SPRINT5/EscapeTheMazeOOP/MazeApp.java
+++ src/main/java/org/example/fromsaif/SPRINT5/EscapeTheMazeOOP/MazeApp.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 
 public class MazeApp {
     public static void main(String[] args) {
-
+        if (args.length != 1) {
+            System.out.println("Error: Please provide the correct file path.");
+            return;
+        }
+        String mazeFilePath = args[0];
     }
 }
