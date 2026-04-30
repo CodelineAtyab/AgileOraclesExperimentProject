f7b9ea2d53f36943908b9da459bf0760fa9a0ecd
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-04-30 15:08:30 +0400

    Add Position class for snake coordinates

--- src/main/java/org/example/fromsaif/SPRINT5/CliSnakeMovementGameOOP/Position.java
+++ src/main/java/org/example/fromsaif/SPRINT5/CliSnakeMovementGameOOP/Position.java
@@ -1,4 +1,16 @@
 package org.example.fromsaif.SPRINT5.CliSnakeMovementGameOOP;
 
 public class Position {
+    int row;
+    int col;
+
+    // Creates a new position on the grid
+    Position(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public String toFileLine() {
+        return row + "," + col;
+    }
 }
