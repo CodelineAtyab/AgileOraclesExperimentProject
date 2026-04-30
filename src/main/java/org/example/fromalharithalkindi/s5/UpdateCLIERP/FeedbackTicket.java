7ede48c7eefd4f7f6194e1073388a3d6f60095a0
AL-Harith AL-Kindi <kindaking99@gmail.com>
2026-04-30 11:56:49 +0400

    added classes

---
+++ src/main/java/org/example/fromalharithalkindi/s5/UpdateCLIERP/FeedbackTicket.java
@@ -0,0 +1,4 @@
+package org.example.fromalharithalkindi.s5.UpdateCLIERP;
+
+public class FeedbackTicket {
+}
---
+++ src/main/java/org/example/fromalharithalkindi/s5/UpdateCLIERP/TicketTest.java
@@ -0,0 +1,7 @@
+package org.example.fromalharithalkindi.s5.UpdateCLIERP;
+
+public class TicketTest {
+    public static void main(String[] args) {
+
+    }
+}
