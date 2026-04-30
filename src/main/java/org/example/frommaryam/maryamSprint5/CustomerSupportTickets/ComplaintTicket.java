ababbe0ce87b034860f726fea983586ebe16d70a
MaryamGit˚🍮🥄 ˚₊‧ <maryamofficially24@gmail.com>
2026-04-30 21:13:43 +0400

    Add fields and counter to ComplaintTicket

--- src/main/java/org/example/frommaryam/maryamSprint5/CustomerSupportTickets/ComplaintTicket.java
+++ src/main/java/org/example/frommaryam/maryamSprint5/CustomerSupportTickets/ComplaintTicket.java
@@ -1,8 +1,20 @@
 package org.example.frommaryam.maryamSprint5.CustomerSupportTickets;
 
 public class ComplaintTicket extends Ticket {
+    private String type;
+    private static int numberOfTickets = 0;
 
-    public ComplaintTicket(String id, String comment) {
+    public ComplaintTicket(String id, String comment, String type) {
         super(id, comment);
+        this.type = type;
+        numberOfTickets++;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public static int getNumberOfTickets() {
+        return numberOfTickets;
     }
 }
