5f8cd6004992436f86379eccd0023946f46e1dac
MaryamGit˚🍮🥄 ˚₊‧ <maryamofficially24@gmail.com>
2026-04-30 21:11:07 +0400

    Make Ticket abstract and add getters

--- src/main/java/org/example/frommaryam/maryamSprint5/CustomerSupportTickets/Ticket.java
+++ src/main/java/org/example/frommaryam/maryamSprint5/CustomerSupportTickets/Ticket.java
@@ -1,11 +1,19 @@
 package org.example.frommaryam.maryamSprint5.CustomerSupportTickets;
 
-public class Ticket {
+public abstract class Ticket {
     private String id;
     private String comment;
 
     public Ticket(String id, String comment) {
         this.id = id;
         this.comment = comment;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getComment() {
+        return comment;
     }
 }
