bf1ecc282fef8f4521ad643b8250272f7b0fdfa8
MaryamGit˚🍮🥄 ˚₊‧ <maryamofficially24@gmail.com>
2026-04-30 21:46:35 +0400

    Add UUID import to ComplaintTicket

--- src/main/java/org/example/frommaryam/maryamSprint5/MiniHelpDeskModule/ComplaintTicket.java
+++ src/main/java/org/example/frommaryam/maryamSprint5/MiniHelpDeskModule/ComplaintTicket.java
@@ -1,5 +1,7 @@
 package org.example.frommaryam.maryamSprint5.MiniHelpDeskModule;
 
+import java.util.UUID;
+
 public class ComplaintTicket extends Ticket {
 
     public ComplaintTicket(String comment) {
