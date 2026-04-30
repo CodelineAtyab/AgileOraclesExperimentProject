cd7540e1c11e2eed56c22127267fb835b5596dc3
MaryamGit˚🍮🥄 ˚₊‧ <maryamofficially24@gmail.com>
2026-04-30 21:43:13 +0400

    Add Ticket constructor

--- src/main/java/org/example/frommaryam/maryamSprint5/MiniHelpDeskModule/Ticket.java
+++ src/main/java/org/example/frommaryam/maryamSprint5/MiniHelpDeskModule/Ticket.java
@@ -5,4 +5,9 @@ import java.time.LocalDateTime;
 public abstract class Ticket {
     private String comment;
     private LocalDateTime createdAt;
+
+    public Ticket(String comment) {
+        this.comment = comment;
+        this.createdAt = LocalDateTime.now();
+    }
 }
