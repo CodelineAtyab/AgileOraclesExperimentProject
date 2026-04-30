608f3cd14ef989f9c9e6b09bad745e22f2c4ca67
MaryamGit˚🍮🥄 ˚₊‧ <maryamofficially24@gmail.com>
2026-04-30 21:45:56 +0400

    Create ComplaintTicket class

---
+++ src/main/java/org/example/frommaryam/maryamSprint5/MiniHelpDeskModule/ComplaintTicket.java
@@ -0,0 +1,13 @@
+package org.example.frommaryam.maryamSprint5.MiniHelpDeskModule;
+
+public class ComplaintTicket extends Ticket {
+
+    public ComplaintTicket(String comment) {
+        super(comment);
+    }
+
+    @Override
+    public String getId() {
+        return "";
+    }
+}
