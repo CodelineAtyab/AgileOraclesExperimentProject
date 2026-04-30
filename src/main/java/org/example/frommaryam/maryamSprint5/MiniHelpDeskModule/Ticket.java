2fe58c3a31cb5f33363b995c46b3e3080ab9476f
MaryamGit˚🍮🥄 ˚₊‧ <maryamofficially24@gmail.com>
2026-04-30 21:42:35 +0400

    Add Ticket common fields

--- src/main/java/org/example/frommaryam/maryamSprint5/MiniHelpDeskModule/Ticket.java
+++ src/main/java/org/example/frommaryam/maryamSprint5/MiniHelpDeskModule/Ticket.java
@@ -3,5 +3,6 @@ package org.example.frommaryam.maryamSprint5.MiniHelpDeskModule;
 import java.time.LocalDateTime;
 
 public abstract class Ticket {
-
+    private String comment;
+    private LocalDateTime createdAt;
 }
