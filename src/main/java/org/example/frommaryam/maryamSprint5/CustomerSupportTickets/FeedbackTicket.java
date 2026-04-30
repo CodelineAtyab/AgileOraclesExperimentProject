fbfddef111b6f171e95fd947b626ee7cbd4526ee
MaryamGit˚🍮🥄 ˚₊‧ <maryamofficially24@gmail.com>
2026-04-30 21:15:18 +0400

    Create FeedbackTicket class

---
+++ src/main/java/org/example/frommaryam/maryamSprint5/CustomerSupportTickets/FeedbackTicket.java
@@ -0,0 +1,9 @@
+package org.example.frommaryam.maryamSprint5.CustomerSupportTickets;
+
+public class FeedbackTicket extends Ticket {
+
+    public FeedbackTicket(String id, String comment) {
+        super(id, comment);
+    }
+}
+
