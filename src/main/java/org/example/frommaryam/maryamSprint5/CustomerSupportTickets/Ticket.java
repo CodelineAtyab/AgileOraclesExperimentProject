b7610596f632dff544c283b5f651da90275d930a
MaryamGit˚🍮🥄 ˚₊‧ <maryamofficially24@gmail.com>
2026-04-30 21:10:24 +0400

    Add constructor to Ticket

--- src/main/java/org/example/frommaryam/maryamSprint5/CustomerSupportTickets/Ticket.java
+++ src/main/java/org/example/frommaryam/maryamSprint5/CustomerSupportTickets/Ticket.java
@@ -3,4 +3,9 @@ package org.example.frommaryam.maryamSprint5.CustomerSupportTickets;
 public class Ticket {
     private String id;
     private String comment;
+
+    public Ticket(String id, String comment) {
+        this.id = id;
+        this.comment = comment;
+    }
 }
