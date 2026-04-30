ab629be797830fbdccf783169449f51154f97a85
MaryamGit˚🍮🥄 ˚₊‧ <maryamofficially24@gmail.com>
2026-04-30 21:47:54 +0400

    Create FeedbackTicket class

---
+++ src/main/java/org/example/frommaryam/maryamSprint5/MiniHelpDeskModule/FeedbackTicket.java
@@ -0,0 +1,13 @@
+package org.example.frommaryam.maryamSprint5.MiniHelpDeskModule;
+
+public class FeedbackTicket extends Ticket {
+
+    public FeedbackTicket(String comment) {
+        super(comment);
+    }
+
+    @Override
+    public String getId() {
+        return "";
+    }
+}
