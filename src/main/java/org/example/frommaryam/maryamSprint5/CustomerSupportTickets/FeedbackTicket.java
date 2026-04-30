445973a1549a86354a71929fe5e0ea996c95ddb3
MaryamGit˚🍮🥄 ˚₊‧ <maryamofficially24@gmail.com>
2026-04-30 21:16:03 +0400

    Add rating validation to FeedbackTicket

--- src/main/java/org/example/frommaryam/maryamSprint5/CustomerSupportTickets/FeedbackTicket.java
+++ src/main/java/org/example/frommaryam/maryamSprint5/CustomerSupportTickets/FeedbackTicket.java
@@ -1,9 +1,20 @@
 package org.example.frommaryam.maryamSprint5.CustomerSupportTickets;
 
 public class FeedbackTicket extends Ticket {
+    private int rating;
 
-    public FeedbackTicket(String id, String comment) {
+    public FeedbackTicket(String id, String comment, int rating) {
         super(id, comment);
+
+        if (rating < 1 || rating > 5) {
+            throw new IllegalArgumentException("Rating must be from 1 to 5");
+        }
+
+        this.rating = rating;
+    }
+
+    public int getRating() {
+        return rating;
     }
 }
 
