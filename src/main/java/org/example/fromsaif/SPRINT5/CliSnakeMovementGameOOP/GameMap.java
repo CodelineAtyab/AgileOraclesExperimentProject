24335a32eb25ce5152154012d6e98a9cff9e98e3
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-04-30 15:40:53 +0400

    Add basic GameMap grid structure

--- src/main/java/org/example/fromsaif/SPRINT5/CliSnakeMovementGameOOP/GameMap.java
+++ src/main/java/org/example/fromsaif/SPRINT5/CliSnakeMovementGameOOP/GameMap.java
@@ -1,4 +1,24 @@
 package org.example.fromsaif.SPRINT5.CliSnakeMovementGameOOP;
 
 public class GameMap {
+
+    // Stored 2D array grid
+    private char[][] grid;
+
+    // Creates a GameMap object using an existing grid
+    public GameMap(char[][] grid) {
+        this.grid = grid;
+    }
+    // Returns the number of rows in the map
+    public int getRows() {
+        return grid.length;
+    }
+    //Returns the number of columns in the map
+    public int getCols() {
+        return grid[0].length;
+    }
+    // Returns the full grid
+    public char[][] getGrid() {
+        return grid;
+    }
 }
