c7e2ca5c7c9dd4776dea69ffa559a6d1bad81f45
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-05-02 23:42:35 +0400

    Add GameRenderer

--- src/main/java/org/example/fromsaif/SPRINT5/CliSnakeMovementGameOOP/GameRenderer.java
+++ src/main/java/org/example/fromsaif/SPRINT5/CliSnakeMovementGameOOP/GameRenderer.java
@@ -1,4 +1,15 @@
 package org.example.fromsaif.SPRINT5.CliSnakeMovementGameOOP;
 
 public class GameRenderer {
+    public void render(GameMap gameMap) {
+        char[][] grid = gameMap.getGrid();
+
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[row].length; col++) {
+                System.out.print(grid[row][col] + " ");
+
+            }
+            System.out.println();
+        }
+    }
 }
