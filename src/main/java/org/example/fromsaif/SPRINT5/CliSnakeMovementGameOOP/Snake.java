149bb1f31f636d5940230f006489f652acfea15d
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-05-02 23:52:01 +0400

    Add basic Snake body structure

--- src/main/java/org/example/fromsaif/SPRINT5/CliSnakeMovementGameOOP/Snake.java
+++ src/main/java/org/example/fromsaif/SPRINT5/CliSnakeMovementGameOOP/Snake.java
@@ -1,4 +1,24 @@
 package org.example.fromsaif.SPRINT5.CliSnakeMovementGameOOP;
 
+import java.util.LinkedList;
+
 public class Snake {
+
+    private LinkedList<Position> body;
+
+    public Snake(LinkedList<Position> body) {
+        this.body = body;
+    }
+
+    public LinkedList<Position> getBody() {
+        return body;
+    }
+
+    public Position getHead() {
+        return body.getLast();
+    }
+
+    public Position getTail() {
+        return body.getFirst();
+    }
 }
