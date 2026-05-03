1e6e42b40095a8895703a46f3b8d6ac922808d3f
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-05-03 14:28:59 +0400

    Add snake head position calculation

--- src/main/java/org/example/fromsaif/SPRINT5/CliSnakeMovementGameOOP/Snake.java
+++ src/main/java/org/example/fromsaif/SPRINT5/CliSnakeMovementGameOOP/Snake.java
@@ -4,24 +4,30 @@ import java.util.LinkedList;
 
 public class Snake {
 
+    // Stores the snake body in order
     private LinkedList<Position> body;
 
+    // Creates a Snake object using existing body list
     public Snake(LinkedList<Position> body) {
         this.body = body;
     }
 
+    //Returns the full snake object
     public LinkedList<Position> getBody() {
         return body;
     }
 
+    // Returns the snake head
     public Position getHead() {
         return body.getLast();
     }
 
+    // Returns the snake tail
     public Position getTail() {
         return body.getFirst();
     }
 
+    // Creates a Snake object by scanning the map for 'o' characters.
     public static Snake fromMap(GameMap gameMap) {
         LinkedList<Position> snakeBody = new LinkedList<>();
         char[][] grid = gameMap.getGrid();
@@ -34,5 +40,24 @@ public class Snake {
             }
         }
         return new Snake(snakeBody);
+    }
+
+    // Calculate where the new head position will be (Snake Head position)
+    public Position calculateNewHead(Direction direction) {
+        Position currentHead = getHead();
+
+        int newRow = currentHead.row;
+        int newCol = currentHead.col;
+
+        if (direction.getValue().equals(Direction.UP)) {
+            newRow--;
+        } else if (direction.getValue().equals(Direction.DOWN)) {
+            newRow++;
+        } else if (direction.getValue().equals(Direction.LEFT)) {
+            newCol--;
+        } else if (direction.getValue().equals(Direction.RIGHT)) {
+            newCol++;
+        }
+        return new Position(newRow, newCol);
     }
 }
