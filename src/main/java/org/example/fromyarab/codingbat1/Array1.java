607b308681f21573905c593d36803a8d48c9594a
Ya'Rab Al-Mamari <115209826+JuniorPrice@users.noreply.github.com>
2026-05-04 08:40:56 +0400

    plusTwo

--- src/main/java/org/example/fromyarab/codingbat1/Array1.java
+++ src/main/java/org/example/fromyarab/codingbat1/Array1.java
@@ -87,4 +87,8 @@ public class Array1 {
         int mid = nums.length / 2;
         return new int[]{nums[mid - 1], nums[mid]};
     }
+
+    public int[] plusTwo(int[] a, int[] b) {
+        return new int[]{a[0], a[1], b[0], b[1]};
+    }
 }
