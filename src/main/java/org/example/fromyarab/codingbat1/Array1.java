507cded14371806e4ba0bfe9898f251ac89fdce2
Ya'Rab Al-Mamari <115209826+JuniorPrice@users.noreply.github.com>
2026-05-04 08:32:56 +0400

    sum2

--- src/main/java/org/example/fromyarab/codingbat1/Array1.java
+++ src/main/java/org/example/fromyarab/codingbat1/Array1.java
@@ -33,4 +33,10 @@ public class Array1 {
         int max = Math.max(nums[0], nums[2]);
         return new int[]{max, max, max};
     }
+
+    public int sum2(int[] nums) {
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        return nums[0] + nums[1];
+    }
 }
