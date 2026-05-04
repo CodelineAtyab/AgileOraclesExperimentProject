fbe5b325eccdfe062500e68c4717a85fe6d2b856
Ya'Rab Al-Mamari <115209826+JuniorPrice@users.noreply.github.com>
2026-05-04 08:27:45 +0400

    makePi

--- src/main/java/org/example/fromyarab/codingbat1/Array1.java
+++ src/main/java/org/example/fromyarab/codingbat1/Array1.java
@@ -8,4 +8,8 @@ public class Array1 {
     public boolean sameFirstLast(int[] nums) {
         return nums.length >= 1 && nums[0] == nums[nums.length - 1];
     }
+
+    public int[] makePi() {
+        return new int[]{3, 1, 4};
+    }
 }
