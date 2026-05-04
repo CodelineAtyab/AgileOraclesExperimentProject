c34604303d2eff207fca0ede2d6fbc7b3b8036cd
Ya'Rab Al-Mamari <115209826+JuniorPrice@users.noreply.github.com>
2026-05-04 08:31:07 +0400

    rotateLeft3

--- src/main/java/org/example/fromyarab/codingbat1/Array1.java
+++ src/main/java/org/example/fromyarab/codingbat1/Array1.java
@@ -20,4 +20,8 @@ public class Array1 {
     public int sum3(int[] nums) {
         return nums[0] + nums[1] + nums[2];
     }
+
+    public int[] rotateLeft3(int[] nums) {
+        return new int[]{nums[1], nums[2], nums[0]};
+    }
 }
