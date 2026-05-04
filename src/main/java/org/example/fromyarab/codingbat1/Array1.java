4e4ecd774a41d9910b36bc62977883515b830e6f
Ya'Rab Al-Mamari <115209826+JuniorPrice@users.noreply.github.com>
2026-05-04 08:44:25 +0400

    unlucky1

--- src/main/java/org/example/fromyarab/codingbat1/Array1.java
+++ src/main/java/org/example/fromyarab/codingbat1/Array1.java
@@ -115,4 +115,11 @@ public class Array1 {
         for (int i = 0; i < size; i++) result[i] = nums[i];
         return result;
     }
+
+    public boolean unlucky1(int[] nums) {
+        if (nums.length >= 2 && nums[0] == 1 && nums[1] == 3) return true;
+        if (nums.length >= 3 && nums[nums.length - 2] == 1 && nums[nums.length - 1] == 3) return true;
+        if (nums.length >= 2 && nums[1] == 1 && nums[2] == 3) return true;
+        return false;
+    }
 }
