2911e8d6a1550767aa38cec402bed4608acff2c6
Ya'Rab Al-Mamari <115209826+JuniorPrice@users.noreply.github.com>
2026-05-04 08:34:24 +0400

    makeEnds

--- src/main/java/org/example/fromyarab/codingbat1/Array1.java
+++ src/main/java/org/example/fromyarab/codingbat1/Array1.java
@@ -43,4 +43,8 @@ public class Array1 {
     public int[] middleWay(int[] a, int[] b) {
         return new int[]{a[1], b[1]};
     }
+
+    public int[] makeEnds(int[] nums) {
+        return new int[]{nums[0], nums[nums.length - 1]};
+    }
 }
