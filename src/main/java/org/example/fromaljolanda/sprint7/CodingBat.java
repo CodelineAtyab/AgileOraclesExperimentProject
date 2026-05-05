fd2f448059471c354e3193a948b8fe0d4d5daf16
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-05-05 07:02:55 +0400

    sleepIn

---
+++ src/main/java/org/example/fromaljolanda/sprint7/CodingBat.java
@@ -0,0 +1,11 @@
+package org.example.fromaljolanda.sprint7;
+
+public class CodingBat {
+    public boolean sleepIn (boolean weekday, boolean vacation) {
+        if (!weekday || vacation) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
