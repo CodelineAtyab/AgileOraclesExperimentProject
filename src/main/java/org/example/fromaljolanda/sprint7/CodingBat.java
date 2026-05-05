a5709925706c1aaea03b368ca9a391c9b02d25b1
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-05-05 07:07:12 +0400

    parrotTrouble

--- src/main/java/org/example/fromaljolanda/sprint7/CodingBat.java
+++ src/main/java/org/example/fromaljolanda/sprint7/CodingBat.java
@@ -38,4 +38,12 @@ public class CodingBat {
         }
     }
 
+    public boolean parrotTrouble (boolean talking, int hour) {
+        if (talking && (hour < 7 || hour > 20)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
