6551edc3c12e8040032e9f239377972df98d0c4e
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-05-05 07:08:03 +0400

    makes10

--- src/main/java/org/example/fromaljolanda/sprint7/CodingBat.java
+++ src/main/java/org/example/fromaljolanda/sprint7/CodingBat.java
@@ -46,4 +46,13 @@ public class CodingBat {
             return false;
         }
     }
+
+    public boolean makes10 (int a, int b) {
+        if (a == 10 || b == 10 || a + b == 10) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
