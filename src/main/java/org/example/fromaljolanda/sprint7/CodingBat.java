110ae48a0c6cddd804e50a29a03a3960819f6eac
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-05-05 07:05:20 +0400

    monkeyTrouble

--- src/main/java/org/example/fromaljolanda/sprint7/CodingBat.java
+++ src/main/java/org/example/fromaljolanda/sprint7/CodingBat.java
@@ -8,4 +8,16 @@ public class CodingBat {
             return false;
         }
     }
+
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if (aSmile && bSmile) {
+            return true;
+        }
+        else if (!aSmile && !bSmile) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
