d726951a44a188bbea1b8eb793546297a85152ba
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-05-05 14:40:16 +0400

    makeOutWord

--- src/main/java/org/example/fromaljolanda/CodingBat.java
+++ src/main/java/org/example/fromaljolanda/CodingBat.java
@@ -98,4 +98,8 @@ public class CodingBat {
     public String makeTags (String tag, String word) {
         return "<" + tag + ">" + word + "</" + tag + ">";
     }
+
+    public String makeOutWord (String out, String word) {
+        return out.substring(0, 2) + word + out.substring(2);
+    }
 }
