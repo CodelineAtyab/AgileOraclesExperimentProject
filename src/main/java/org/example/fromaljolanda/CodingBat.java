888233262608448e6c79f7406a96db1af7ca2f06
Al-Jolanda Al-Handhali <aljolandaalhandhali7@gmail.com>
2026-05-05 07:14:53 +0400

    posNeg

--- src/main/java/org/example/fromaljolanda/CodingBat.java
+++ src/main/java/org/example/fromaljolanda/CodingBat.java
@@ -66,5 +66,17 @@ public class CodingBat {
         }
     }
 
+    public boolean posNeg(int a, int b, boolean negative) {
+        if (negative == true && a < 0 && b < 0) {
+            return true;
+        }
+        else if (negative == false && ((a < 0 && b > 0) || (a > 0 && b < 0))) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
 
 }
