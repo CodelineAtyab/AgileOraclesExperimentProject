83e6af9c75c6990531802b6d9227cae54adf59da
alharithalk <kindaking99@gmail.com>
2026-05-06 09:49:36 +0400

    FeedbackTicket class Done

--- src/main/java/org/example/fromalharithalkindi/s5/UpdateCLIERP/FeedbackTicket.java
+++ src/main/java/org/example/fromalharithalkindi/s5/UpdateCLIERP/FeedbackTicket.java
@@ -3,4 +3,22 @@ package org.example.fromalharithalkindi.s5.UpdateCLIERP;
 
 public class FeedbackTicket extends Ticket {
 
+    private static int count=0;
+    private int num;
+
+
+    public FeedbackTicket(String title, String description)
+    {
+        super(title, description);
+        this.num = ++count;
+    }
+
+    @Override
+    public String getId()
+    {
+        return String.format("FEED-%03d", num);
+    }
+
+}
+
 
