c3e937f09857c77c28c2c01a8121131a07fb1c34
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-05-06 11:13:37 +0400

    Add encapsulated ComplaintTicket class

--- src/main/java/org/example/fromsaif/SPRINT6/UpdateCliErpEncapsulation/ComplaintTicket.java
+++ src/main/java/org/example/fromsaif/SPRINT6/UpdateCliErpEncapsulation/ComplaintTicket.java
@@ -1,24 +1,30 @@
 package org.example.fromsaif.SPRINT6.UpdateCliErpEncapsulation;
 
-import org.example.fromsaif.SPRINT6.UpdateCliErpPolymorphic.Ticket;
-
-import java.util.UUID;
-
 public class ComplaintTicket extends Ticket {
 
-    // Constructor passes the complaint comment to the parent Ticket class.
-    public ComplaintTicket(String comment) {
-        super(comment);
+    // Stores the complaint type
+    private String type;
+
+    // Counts how many ComplaintTicket objects have been created.
+    private static int numberOfTickets = 0;
+
+    // Constructor creates complaint ticket using ID, comment, type.
+    public ComplaintTicket(String id, String comment, String type) {
+        super(id, comment);
+        this.type = type;
     }
 
-    // Overrides the abstract getId() method from Ticket.
-    @Override
-    public String getId() {
-        return buildPrefix() + UUID.randomUUID().toString();
-    }
+    // Returns the complaint type
+    public String getType() {
+        return type;
 
-    // Private helper method used internally by getId().
-    private String buildPrefix() {
-        return "C-";
+    }
+    // Updates the complaint type
+    public void setType(String type) {
+        this.type = type;
+    }
+    // Returns how many complaint tickets have been created
+    public static int getNumberOfTickets() {
+        return numberOfTickets;
     }
 }
