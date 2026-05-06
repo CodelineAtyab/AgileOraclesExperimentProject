53b0bfb22ceedfc4e5706135824ab593e45f6891
alharithalk <kindaking99@gmail.com>
2026-05-06 09:33:06 +0400

    ticket class Done

--- src/main/java/org/example/fromalharithalkindi/s5/UpdateCLIERP/Ticket.java
+++ src/main/java/org/example/fromalharithalkindi/s5/UpdateCLIERP/Ticket.java
@@ -1,6 +1,38 @@
 package org.example.fromalharithalkindi.s5.UpdateCLIERP;
 
 public abstract  class Ticket {
+    private  String title;
+    private  String description;
+
+
+    public Ticket(String title,String description)
+    {
+        this.title=title;
+        this.description=description;
+
+    }
+
+    public abstract String getId();
+
+    public  String getSummary()
+    {
+        return buildSummary();
+    }
+
+    private String buildSummary()
+    {
+        return "[ "+getId()+" ] ,"+title+" - "+ description;
+
+    }
+    public String getTitle()
+    {
+        return title;
+    }
+    public String getDescription()
+    {
+        return description;
+    }
+
 
 }
 
