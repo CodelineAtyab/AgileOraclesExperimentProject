2f82cf72e374abb9960babc10566a0bc9f2bb2e1
SAIFMOHSIN8 <saif.almahrouqii@gmail.com>
2026-05-06 12:01:15 +0400

    Finished ticket demo

--- src/main/java/org/example/fromsaif/SPRINT6/UpdateCliErpEncapsulation/TicketDemo.java
+++ src/main/java/org/example/fromsaif/SPRINT6/UpdateCliErpEncapsulation/TicketDemo.java
@@ -1,18 +1,52 @@
 package org.example.fromsaif.SPRINT6.UpdateCliErpEncapsulation;
 
-import org.example.fromsaif.SPRINT6.UpdateCliErpPolymorphic.ComplaintTicket;
-import org.example.fromsaif.SPRINT6.UpdateCliErpPolymorphic.FeedbackTicket;
-import org.example.fromsaif.SPRINT6.UpdateCliErpPolymorphic.Ticket;
+import java.util.ArrayList;
 
 public class TicketDemo {
     public static void main(String[] args) {
 
-        org.example.fromsaif.SPRINT6.UpdateCliErpPolymorphic.Ticket complaintTicket = new ComplaintTicket("System is not working properly");
+        // ArrayList stores all ticket types using the parent type Ticket.
+        ArrayList<Ticket> tickets = new ArrayList<>();
 
-        Ticket feedbackTicket = new FeedbackTicket("The ERP system is ready to use");
+        // At the beginning, no ComplaintTicket objects have been created yet.
+        System.out.println("Complaint tickets: " + ComplaintTicket.getNumberOfTickets());
+        System.out.println("--- Creating tickets ---");
 
-        System.out.println("Complaint Ticket ID: " + complaintTicket.getId());
+        // Create complaint tickets.
+        ComplaintTicket complaintOne = new ComplaintTicket("CT-001", "Wifi not connection", "Payment");
+        ComplaintTicket complaintTwo = new ComplaintTicket("CT-002", "Wifi is very slow", "Network");
 
-        System.out.println("Feedback Ticket ID: " + feedbackTicket.getId());
+        // Create feedback ticket.
+        FeedbackTicket feedbackOne = new FeedbackTicket("FB-001", "Staff did amazing job", 5);
+
+        // Add all tickets to the same list.
+        tickets.add(complaintOne);
+        tickets.add(complaintTwo);
+        tickets.add(feedbackOne);
+
+        System.out.println("ComplaintTicket " + complaintOne.getId() + " added.");
+        System.out.println("ComplaintTicket " + complaintTwo.getId() + " added.");
+        System.out.println("FeedbackTicket " + feedbackOne.getId() + " added.");
+
+        // The complaint counter should now be 2 because two ComplaintTicket objects were created.
+        System.out.println("Complaint tickets so far: " + ComplaintTicket.getNumberOfTickets());
+
+        System.out.println("--- Ticket List ---");
+
+        // Print each ticket in the list.
+        for (Ticket ticket : tickets) {
+            if (ticket instanceof ComplaintTicket complaintTicket) {
+                System.out.println("[Complaint] id=" + complaintTicket.getId()
+                        + ", comment=\"" + complaintTicket.getComment()
+                        + "\", type=\"" + complaintTicket.getType() + "\"");
+
+            }
+            else if (ticket instanceof FeedbackTicket feedbackTicket) {
+                System.out.println("[Feedback ] id=" + feedbackTicket.getId()
+                        + ", comment=\"" + feedbackTicket.getComment()
+                        + "\", rating=" + feedbackTicket.getRating());
+
+            }
+        }
     }
 }
