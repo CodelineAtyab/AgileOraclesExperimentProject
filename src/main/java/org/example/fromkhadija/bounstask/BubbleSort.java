ee2d52f23b2a9848ff62030779dc1d41ef02eced
KhadijaAli <alloughanikhadijaali@gmail.com>
2026-05-09 02:46:25 +0400

    update bubble sort class

--- src/main/java/org/example/fromkhadija/bounstask/BubbleSort.java
+++ src/main/java/org/example/fromkhadija/bounstask/BubbleSort.java
@@ -2,15 +2,18 @@ package org.example.fromkhadija.bounstask;
 
 public class BubbleSort implements SortStrategy {
     public void sort(int[] Numbers) {
-        System.out.println("*******************BubbleSort*******************");
+        System.out.println("\n\n*******************BubbleSort*******************");
 
 
-        for (int j = 0; j < Numbers.length - 1; j++) {
-            if (Numbers[j] > Numbers[j + 1]) {
-                //Swap:
-                int temp = Numbers[j];
-                Numbers[j] = Numbers[j + 1];
-                Numbers[j + 1] = temp;
+        for (int i = 0; i < Numbers.length - 1; i++) {
+            for (int j = 0; j < Numbers.length - 1 - i; j++) {
+
+                if (Numbers[j] > Numbers[j + 1]) {
+
+                    int temp = Numbers[j];
+                    Numbers[j] = Numbers[j + 1];
+                    Numbers[j + 1] = temp;
+                }
             }
         }
     }
