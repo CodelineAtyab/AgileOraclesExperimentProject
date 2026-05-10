4ab92e1072adad400c6b41c6465b80c6983194de
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-10 11:52:12 +0400

    added map 2 task

--- src/main/java/org/example/fromtibyan/codingbat/MapTwo.java
+++ src/main/java/org/example/fromtibyan/codingbat/MapTwo.java
@@ -3,5 +3,11 @@ package org.example.fromtibyan.codingbat;
 import java.util.*;
 
 public class MapTwo {
+    public Map<String, Integer> word0(String[] strings) {
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for (String key: strings){
+            map.put(key,0);
+        }return map;
+    }
 
 }
