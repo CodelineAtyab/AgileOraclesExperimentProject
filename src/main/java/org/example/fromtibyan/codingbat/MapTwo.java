5acfe5863661ceb7ec26f41921195661bad668bd
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-10 11:53:24 +0400

    added map 2 task

--- src/main/java/org/example/fromtibyan/codingbat/MapTwo.java
+++ src/main/java/org/example/fromtibyan/codingbat/MapTwo.java
@@ -48,4 +48,17 @@ public class MapTwo {
 
     }
 
+    public String wordAppend(String[] strings) {
+        Map<String, Integer> countMap = new HashMap<>();
+        StringBuilder result = new StringBuilder();
+        for (String key : strings) {
+            int count = countMap.getOrDefault(key, 0) + 1;
+            countMap.put(key, count);
+            if (count % 2 == 0) {
+                result.append(key);
+            }
+        }
+        return result.toString();
+    }
+
 }
