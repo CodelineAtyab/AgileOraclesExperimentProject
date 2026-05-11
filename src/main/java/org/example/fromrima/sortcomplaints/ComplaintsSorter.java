183ee08c8520bd8a942be89a1686be886d7b9670
RimaAhmed1 <rimaaltarshi@gmaile.com>
2026-05-11 17:42:53 +0400

    make sure the code program run probably

---
+++ src/main/java/org/example/fromrima/sortcomplaints/ComplaintsSorter.java
@@ -0,0 +1,7 @@
+package org.example.fromrima.sortcomplaints;
+
+public class ComplaintsSorter {
+    public static void main(String[] args) {
+        System.out.println("Selection and Bubble Sort");
+    }
+}
