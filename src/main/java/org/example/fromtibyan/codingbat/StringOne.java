e18580558fd2092f80f037a1f62c56d44b7729f7
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-13 14:34:32 +0400

    added task

--- src/main/java/org/example/fromtibyan/codingbat/StringOne.java
+++ src/main/java/org/example/fromtibyan/codingbat/StringOne.java
@@ -122,6 +122,21 @@ public class StringOne {
         }return str;
     }
 
+    public boolean hasBad(String str) {
+        if(str.length()>=3){
+            if (str.charAt(0)=='b'&& str.charAt(1)=='a' && str.charAt(2)=='d'){
+                return true;
+            }
+        }
+        if (str.length()>3){
+            if (str.charAt(1)=='b'&& str.charAt(2)=='a' && str.charAt(3)=='d'){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 
 
 }
