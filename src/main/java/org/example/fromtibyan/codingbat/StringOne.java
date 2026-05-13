5e351130ca141cdd8f8f9876f328f544f59fb480
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-13 14:37:04 +0400

    added task

--- src/main/java/org/example/fromtibyan/codingbat/StringOne.java
+++ src/main/java/org/example/fromtibyan/codingbat/StringOne.java
@@ -184,6 +184,23 @@ public class StringOne {
         } return "";
     }
 
+    public boolean frontAgain(String str) {
+        if (str.length()==2){
+            return true;
+        }else if (str.length() >=3){
+            if(str.charAt(0)==str.charAt(str.length()-2) && str.charAt(1)==str.charAt(str.length()-1)){
+                return true;
+            }
+        }return false;
+    }
+
+    public String minCat(String a, String b) {
+        int minLen = Math.min(a.length(), b.length());
+        String aTrimmed = a.substring(a.length() - minLen);
+        String bTrimmed = b.substring(b.length() - minLen);
+        return aTrimmed + bTrimmed;
+    }
+
 
 
 
