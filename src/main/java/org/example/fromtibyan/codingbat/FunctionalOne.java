ed61d80d1295cdd251001e6df193a6c4f0acccad
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-13 15:30:22 +0400

    added task

--- src/main/java/org/example/fromtibyan/codingbat/FunctionalOne.java
+++ src/main/java/org/example/fromtibyan/codingbat/FunctionalOne.java
@@ -34,5 +34,9 @@ public class FunctionalOne {
         return nums;
     }
 
+    public List<Integer> rightDigit(List<Integer> nums) {
+        nums.replaceAll(n -> n%10);
+        return nums;
+    }
 
 }
