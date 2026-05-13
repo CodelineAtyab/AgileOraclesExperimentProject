238cba54a3f9861639ab975634e0db238a459ba0
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-13 14:37:18 +0400

    added task

--- src/main/java/org/example/fromtibyan/codingbat/StringOne.java
+++ src/main/java/org/example/fromtibyan/codingbat/StringOne.java
@@ -201,6 +201,15 @@ public class StringOne {
         return aTrimmed + bTrimmed;
     }
 
+    public String extraFront(String str) {
+        String front;
+        if (str.length() < 2) {
+            front = str;
+        } else {
+            front = str.substring(0, 2);
+        } return front + front + front;
+    }
+
 
 
 
