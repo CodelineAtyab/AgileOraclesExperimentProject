64cd1a2d16472f4aa73b04e264aafc90c634f685
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-13 15:24:44 +0400

    added task

--- src/main/java/org/example/fromtibyan/codingbat/FunctionalOne.java
+++ src/main/java/org/example/fromtibyan/codingbat/FunctionalOne.java
@@ -19,5 +19,10 @@ public class FunctionalOne {
         return strings;
     }
 
+    public List<String> copies3(List<String> strings) {
+        strings.replaceAll(s -> s+s+s);
+        return strings;
+    }
+
 
 }
