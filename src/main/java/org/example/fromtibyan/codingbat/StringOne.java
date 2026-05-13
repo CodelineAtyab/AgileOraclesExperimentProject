7af1e7e96786bea69e30e5833188a1b4a46ce967
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-13 14:30:53 +0400

    added task

--- src/main/java/org/example/fromtibyan/codingbat/StringOne.java
+++ src/main/java/org/example/fromtibyan/codingbat/StringOne.java
@@ -90,5 +90,14 @@ public class StringOne {
 
     }
 
+    public boolean endsLy(String str) {
+        if (str.length()>1){
+            if (str.charAt(str.length()-1) == 'y'
+                    && str.charAt(str.length()-2) == 'l'){
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
