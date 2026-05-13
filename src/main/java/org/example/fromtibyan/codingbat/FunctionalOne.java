c4306181f59c5f197287865c1f7969948189be72
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-13 15:22:20 +0400

    added task

--- src/main/java/org/example/fromtibyan/codingbat/FunctionalOne.java
+++ src/main/java/org/example/fromtibyan/codingbat/FunctionalOne.java
@@ -1,4 +1,13 @@
 package org.example.fromtibyan.codingbat;
 
+import java.util.List;
+
 public class FunctionalOne {
+
+    public List<Integer> doubling(List<Integer> nums) {
+        nums.replaceAll(n->n*2);
+        return nums;
+    }
+
+
 }
