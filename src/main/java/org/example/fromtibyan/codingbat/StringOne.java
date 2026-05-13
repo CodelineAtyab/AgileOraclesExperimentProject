de05252f47f166fe0cd42cde7f1c2cdc2206a19c
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-13 14:29:55 +0400

    added task

--- src/main/java/org/example/fromtibyan/codingbat/StringOne.java
+++ src/main/java/org/example/fromtibyan/codingbat/StringOne.java
@@ -65,6 +65,13 @@ public class StringOne {
         }return str;
     }
 
+    public String theEnd(String str, boolean front) {
+        if (front) {
+            return String.valueOf(str.charAt(0));
+        } else {
+            return String.valueOf(str.charAt(str.length() - 1));
+        }
+    }
 
 
 }
