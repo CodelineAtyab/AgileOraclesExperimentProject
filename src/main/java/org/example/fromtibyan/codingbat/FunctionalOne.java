57708bd85916dc0181132daac7fbfda155af4790
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-13 15:30:34 +0400

    added task

--- src/main/java/org/example/fromtibyan/codingbat/FunctionalOne.java
+++ src/main/java/org/example/fromtibyan/codingbat/FunctionalOne.java
@@ -39,4 +39,10 @@ public class FunctionalOne {
         return nums;
     }
 
+    public List<String> lower(List<String> strings) {
+        strings.replaceAll(s -> s.toLowerCase());
+        return strings;
+    }
+
+
 }
