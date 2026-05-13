7af0b7e358978e607913c52a081c06807b783310
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-13 14:36:26 +0400

    added task

--- src/main/java/org/example/fromtibyan/codingbat/StringOne.java
+++ src/main/java/org/example/fromtibyan/codingbat/StringOne.java
@@ -174,6 +174,17 @@ public class StringOne {
         }return str;
     }
 
+    public String seeColor(String str) {
+        if (str.length()>=3){
+            if (str.substring(0,3).equals("red")){
+                return "red";
+            }else if (str.length()>=4 && str.substring(0,4).equals("blue")){
+                return "blue";
+            }
+        } return "";
+    }
+
+
 
 
 }
