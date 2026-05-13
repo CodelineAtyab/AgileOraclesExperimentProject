d8d2812e1406d26fcc9fde1c73f5535c2272fd1f
TibyanSaad <saad.tibyan02@gmail.com>
2026-05-13 15:24:09 +0400

    added task

--- src/main/java/org/example/fromtibyan/codingbat/FunctionalOne.java
+++ src/main/java/org/example/fromtibyan/codingbat/FunctionalOne.java
@@ -9,5 +9,10 @@ public class FunctionalOne {
         return nums;
     }
 
+    public List<Integer> square(List<Integer> nums) {
+        nums.replaceAll(n->n*n);
+        return nums;
+    }
+
 
 }
