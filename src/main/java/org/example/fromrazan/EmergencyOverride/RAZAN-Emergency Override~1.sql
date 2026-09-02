9e316f446aee67a4277488cbbf6c33e8f8b58f56
Razansalam <rooozalharthi@gmail.com>
2026-09-02 10:53:43 +0400

    Emergency-Override

--- src/main/java/org/example/fromrazan/EmergencyOverride/RAZAN-Emergency Override~1.sql
+++ src/main/java/org/example/fromrazan/EmergencyOverride/RAZAN-Emergency Override~1.sql
@@ -1,21 +1,8 @@
-begin
-for signalObj in (select * from traffic_signals) loop
- dbms_output.put_line(signalObj.signal_name);
-end loop;
+SET SERVEROUTPUT ON;
 
-end;
-/
-
-
-
-begin
-for signalObj in (select * from traffic_signals) loop
-update traffic_signals set last_changed_at = systimestamp where signal_id = signalObj.signal_id;
-dbms_output.put_line(signalObj.signal_id || ' ' || signalObj.signal_id || ' ' || signalObj.signal_id );
-end loop;
-
-end;
-/
+------------------------------------------------------------
+-- PART 1: IMPLICIT CURSOR
+------------------------------------------------------------
 
 CREATE OR REPLACE PROCEDURE reset_signals_implicit
 IS
@@ -23,32 +10,35 @@ IS
 BEGIN
 FOR signalObj IN (
         SELECT signal_id
-        FROM traffic_signals  )
+        FROM traffic_signals
+    )
     LOOP
-
 UPDATE traffic_signals
 SET state = 'RED',
     last_changed_at = SYSTIMESTAMP
 WHERE signal_id = signalObj.signal_id;
 
 v_count := v_count + 1;
-
 END LOOP;
-
 COMMIT;
-
-DBMS_OUTPUT.PUT_LINE( 'Implicit cursor - Signals reset: ' || v_count );
-
+DBMS_OUTPUT.PUT_LINE('Implicit cursor - Signals reset: ' || v_count );
 END reset_signals_implicit;
 /
 
+------------------------------------------------------------
+-- PART 2: EXPLICIT CURSOR USING SYS_REFCURSOR
+------------------------------------------------------------
 
-
-
---part2:
-
-SELECT signal_id, signal_name FROM traffic_signals;
-
+CREATE OR REPLACE PROCEDURE get_next_signals (
+    p_cursor OUT SYS_REFCURSOR
+)
+AS
+BEGIN
+OPEN p_cursor FOR
+SELECT *
+FROM traffic_signals;
+END get_next_signals;
+/
 
 CREATE OR REPLACE PROCEDURE reset_signals_explicit
 IS
@@ -57,51 +47,31 @@ IS
     v_count NUMBER := 0;
 BEGIN
     get_next_signals(v_cursor);
+
     LOOP
 FETCH v_cursor INTO v_rec;
+
         EXIT WHEN v_cursor%NOTFOUND;
+
 UPDATE traffic_signals
 SET state = 'RED',
     last_changed_at = SYSTIMESTAMP
 WHERE signal_id = v_rec.signal_id;
 
 v_count := v_count + 1;
-
 END LOOP;
+
 CLOSE v_cursor;
+
 COMMIT;
 
-DBMS_OUTPUT.PUT_LINE('Explicit cursor - Signals reset: ' || v_count);
-
+DBMS_OUTPUT.PUT_LINE( 'Explicit cursor - Signals reset: ' || v_count);
 END reset_signals_explicit;
 /
 
-CREATE OR REPLACE PROCEDURE get_next_signals ( p_cursor OUT SYS_REFCURSOR )
-AS
-BEGIN
-OPEN p_cursor FOR
-SELECT * FROM traffic_signals;
-END get_next_signals;
-
-
-DECLARE
-v_cursor SYS_REFCURSOR;
-    v_rec traffic_signals%ROWTYPE;
-
-BEGIN
-    get_next_signals(v_cursor);
-
-    LOOP
-FETCH v_cursor INTO v_rec;
-        EXIT WHEN v_cursor%NOTFOUND;
-        DBMS_OUTPUT.PUT_LINE( v_rec.signal_id || ' ' ||v_rec.signal_name || ' ' || v_rec.state );
-END LOOP;
-CLOSE v_cursor;
-END;
-/
-
-
---p3Bullk:
+------------------------------------------------------------
+-- PART 3: BULK UPDATE
+------------------------------------------------------------
 
 CREATE OR REPLACE PROCEDURE reset_signals_bulk
 IS
@@ -116,43 +86,53 @@ BEGIN
 OPEN v_cursor FOR
 SELECT signal_id
 FROM traffic_signals;
+
 FETCH v_cursor BULK COLLECT INTO v_signal_ids;
+
 CLOSE v_cursor;
+
 IF v_signal_ids.COUNT > 0 THEN
+
         FORALL i IN 1 .. v_signal_ids.COUNT
+
 UPDATE traffic_signals
 SET state = 'RED',
     last_changed_at = SYSTIMESTAMP
 WHERE signal_id = v_signal_ids(i);
-v_count := SQL%ROWCOUNT;
+
+v_count := v_signal_ids.COUNT;
+
 END IF;
+
 COMMIT;
-DBMS_OUTPUT.PUT_LINE('Bulk update - Signals reset: ' || v_count );
+
+DBMS_OUTPUT.PUT_LINE( 'Bulk update - Signals reset: ' || v_count );
 
 END reset_signals_bulk;
 /
 
-
---test
-SET SERVEROUTPUT ON;
-
+------------------------------------------------------------
+-- TEST ALL PROCEDURES
+------------------------------------------------------------
 
 BEGIN
     reset_signals_implicit;
 END;
 /
 
-
 BEGIN
     reset_signals_explicit;
 END;
 /
 
-
 BEGIN
     reset_signals_bulk;
 END;
 /
+
+------------------------------------------------------------
+-- VERIFY RESULTS
+------------------------------------------------------------
 
 SELECT signal_id,
        signal_name,
@@ -161,9 +141,10 @@ SELECT signal_id,
 FROM traffic_signals
 ORDER BY signal_id;
 
+------------------------------------------------------------
+-- VERIFY PROCEDURES
+------------------------------------------------------------
 
-
----Then check all procedures:
 SELECT object_name,
        object_type,
        status
@@ -175,3 +156,4 @@ WHERE object_name IN (
                       'RESET_SIGNALS_BULK'
     )
 ORDER BY object_name;
+
